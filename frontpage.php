7f13bfa9de2113d0cc872ca9a8ddcaf18493e868
Lars <31955106+lsnielsen@users.noreply.github.com>
2019-08-30 13:14:38 +0200

    Make new pages

--- frontpage.php
+++ frontpage.php
@@ -1,9 +1,15 @@
 <?php
 
 $cvButton = isset($_POST['cvButton']) ? $_POST['cvButton'] : "";
+$carButton = isset($_POST['carButton']) ? $_POST['carButton'] : "";
+$tvButton = isset($_POST['tvButton']) ? $_POST['tvButton'] : "";
 
 if ($cvButton == "cvPage") {
 	echo file_get_contents("view/cv.html");	
+} elseif ($carButton == "carPage") {
+	echo file_get_contents("view/car.html");
+} elseif ($tvButton == "tvPage") {
+	echo file_get_contents("view/tv.html");
 } else {
 	echo file_get_contents("view/frontpage.html");
 }
