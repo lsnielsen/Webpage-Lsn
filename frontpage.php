2844b787dd4aec4d976abc86a483354be01f5415
Lars <31955106+lsnielsen@users.noreply.github.com>
2019-08-30 12:20:26 +0200

    Fix new paths

--- frontpage.php
+++ frontpage.php
@@ -3,9 +3,9 @@
 $cvButton = isset($_POST['cvButton']) ? $_POST['cvButton'] : "";
 
 if ($cvButton == "cvPage") {
-	echo file_get_contents("Webpage-Lsn/view/cv.html");	
+	echo file_get_contents("view/cv.html");	
 } else {
-	echo file_get_contents("Webpage-Lsn/view/frontpage.html");
+	echo file_get_contents("view/frontpage.html");
 }
 
 ?>
