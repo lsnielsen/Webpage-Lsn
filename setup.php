d56a2aa3f3a34f11f8ad88a43aa9bacb5848f8f3
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-07-12 19:50:39 +0200

    Make the new cv button

--- setup.php
+++ setup.php
@@ -3,6 +3,7 @@
 $cvButton = isset($_POST['cvButton']) ? $_POST['cvButton'] : "";
 $carButton = isset($_POST['carButton']) ? $_POST['carButton'] : "";
 $tvButton = isset($_POST['tvButton']) ? $_POST['tvButton'] : "";
+$newCvButton = isset($_POST['newCvButton']) ? $_POST['newCvButton'] : "";
 
 if ($cvButton == "cvPage") {
 	echo file_get_contents("view/cv.html");	
@@ -10,6 +11,8 @@ if ($cvButton == "cvPage") {
 	echo file_get_contents("view/car.html");
 } elseif ($tvButton == "tvPage") {
 	echo file_get_contents("view/tv.html");
+} elseif ($newCvButton == "newCvPage") {
+	echo file_get_contents("view/cv.html");
 } else {
 	echo file_get_contents("view/frontpage.html");
 }
