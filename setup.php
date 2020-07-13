6332379a24af91db2f1b525ca19e0dd81c5ec9b3
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-07-13 14:09:34 +0200

    Direct to the new page

--- setup.php
+++ setup.php
@@ -3,6 +3,7 @@
 $carButton = isset($_POST['carButton']) ? $_POST['carButton'] : "";
 $cvButton = isset($_POST['cvButton']) ? $_POST['cvButton'] : "";
 $tvButton = isset($_POST['tvButton']) ? $_POST['tvButton'] : "";
+$dieselButton = isset($_POST['dieselButton']) ? $_POST['dieselButton'] : "";
 
 if ($cvButton == "cvPage") {
 	echo file_get_contents("view/cv.html");	
@@ -10,6 +11,8 @@ if ($cvButton == "cvPage") {
 	echo file_get_contents("view/car.html");
 } elseif ($tvButton == "tvPage") {
 	echo file_get_contents("view/tv.html");
+} elseif ($dieselButton == "dieselPage") {
+	echo file_get_contents("view/diesel.html");
 } else {
 	echo file_get_contents("view/frontpage.html");
 }
