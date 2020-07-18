d9d408a7565ba65ba8c00cb193468faa50753931
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-07-18 10:42:07 +0200

    Direct the new links

--- setup.php
+++ setup.php
@@ -7,9 +7,16 @@ $cvButton = isset($_POST['cvButton']) ? $_POST['cvButton'] : "";
 if ($cvButton == "cvPage") {
 	echo file_get_contents("view/cv/cv.html");	
 } elseif ($cvButton == "educationCv") {
-	echo file_get_contents("view/cv/html/education/educationCv.html");		
+	echo file_get_contents("view/cv/html/education/educationCv.html");	
+	echo "testing";
 } elseif ($cvButton == "sparetimeCv") {
 	echo file_get_contents("view/cv/html/sparetime/sparetimeCv.html");	
+} elseif ($cvButton == "qualificationCv") {
+	echo file_get_contents("view/cv/html/qualifications/qualificationsCv.html");	
+} elseif ($cvButton == "languageCv") {
+	echo file_get_contents("view/cv/html/language/languageCv.html");		
+} elseif ($cvButton == "otherCv") {
+	echo file_get_contents("view/cv/html/other/otherCv.html");	
 } elseif ($carButton == "carPage") {
 	echo file_get_contents("view/car.html");
 } elseif ($tvButton == "tvPage") {
