2bf1635930e7c85212b5c7c51351f71a471ebe22
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-07-20 15:52:40 +0200

    Move language files

--- setup.php
+++ setup.php
@@ -7,7 +7,7 @@ $cvButton = isset($_POST['cvButton']) ? $_POST['cvButton'] : "";
 if ($cvButton == "cvPage") {
 	echo file_get_contents("view/cv/cv.html");	
 } elseif ($cvButton == "educationCv") {
-	echo file_get_contents("view/cv/html/education/educationCv.html");	
+	echo file_get_contents("view/cv/education/educationCv.html");	
 } elseif ($cvButton == "sparetimeCv") {
 	echo file_get_contents("view/cv/sparetime/sparetimeCv.html");	
 } elseif ($cvButton == "qualificationCv") {
