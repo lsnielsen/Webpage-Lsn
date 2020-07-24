a67c64633a6a9b815cd84148a218be9c20134724
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-07-24 17:06:39 +0200

    Update graph.php

--- view/diesel/graph.php
+++ view/diesel/graph.php
@@ -1,43 +1,41 @@
 
-		<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-		<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-
-<script>
-	window.onload = function () {
 
 
-		var chart = new CanvasJS.Chart("chartContainer", {
-			animationEnabled: true,
-			theme: "light2",
-			title:{
-				text: "Simple Line Chart"
-			},
-			axisY:{
-				includeZero: false
-			},
-			data: [{        
-				type: "line",
-				indexLabelFontSize: 16,
-				dataPoints: [
-					{ y: 450 },
-					{ y: 414},
-					{ y: 520, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" },
-					{ y: 460 },
-					{ y: 450 },
-					{ y: 500 },
-					{ y: 480 },
-					{ y: 480 },
-					{ y: 410 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-					{ y: 500 },
-					{ y: 480 },
-					{ y: 510 }
-				]
-			}]
-		});
-		chart.render();
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
 
-	}
-</script>
+
+<?php
+	
+	$graphData = "SELECT * FROM diesel";
+	$result = mysqli_query($con,$graphData);
+
+?>
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Dato', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
 	
 	
 	
