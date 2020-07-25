2ae6e21e3f23e940357118bdd556d1c225c24d5b
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-07-25 11:12:43 +0200

    Update graph.php

--- view/diesel/graph.php
+++ view/diesel/graph.php
@@ -1,42 +1,14 @@
 
-<div id="curve_chart" style="width: 900px; height: 500px"></div>	
-	
+<center>
+	<div id="curve_chart" style="width: 2100px; height: 700px; margin-left: -110px;"></div>	
+</center>	
 
 <?php
 	
 	$graphData = "SELECT * FROM diesel";
 	$result = mysqli_query($con,$graphData);
-	$resultII = mysqli_query($con,$graphData);
-	$i = 0;
-	while ($rowArray = $resultII->fetch_array()) {
-		$array[] = $rowArray;
-	}
-	while($row = $result->fetch_array()){
-		echo "<div display:none class=db"; 
-		echo json_encode($i); 
-		echo " value=";
-		echo json_encode($row['date']);
-		echo "> </div>";
-
-		echo "<div display:none class=db"; 
-		echo json_encode($i); 
-		echo " value=";
-		echo json_encode($row['kilometer']);
-		echo "> </div>";
-
-		echo "<div display:none class=db"; 
-		echo json_encode($i); 
-		echo " value=";
-		echo json_encode($row['liter']);
-		echo "> </div>";
-
-		echo "<div display:none class=db"; 
-		echo json_encode($i); 
-		echo " value=";
-		echo json_encode($row['kroner']);
-		echo "> </div>";
-
-		$i++;
+	while ($rowArray = $result->fetch_array()) {
+		$graphArray[] = $rowArray;
 	}
 ?>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
@@ -46,20 +18,12 @@
 	google.charts.load('current', {'packages':['corechart']});
 	google.charts.setOnLoadCallback(drawChart);
 
-	var i = <?php echo $i ?>;
-	var j = 0;
-	
-
-	var temp = jQuery.parseJSON('<?php echo json_encode($array) ?>');
-	console.log(temp);
+	var graphArray = jQuery.parseJSON('<?php echo json_encode($graphArray) ?>');
 
 	function drawChart() {
 		var data = google.visualization.arrayToDataTable([
 		  ['Dato', 'kilometer', 'kroner', 'liter'],
-		  ['2004',  1000, 400, 23],
-		  ['2005',  1170, 460, 43],
-		  ['2006',  660, 1120, 32],
-		  ['2007',  1030, 540, 89]
+		  ['2018-07-25', 0, 0, 0]
 		]);
 
 		var options = {
@@ -67,14 +31,15 @@
 		  curveType: 'function',
 		  legend: { position: 'bottom' }
 		};
-		<?php 
-			echo "data.addRow([";
-			echo " '2008',";
-			echo "562,";
-			echo "456,";
-			echo "654";
-			echo "]);";
-		?>
+		var arrayLength = graphArray.length;
+		for (var i = 0; i < arrayLength; i++) {
+			data.addRow([
+				graphArray[i][1],
+				parseInt(graphArray[i][2]),
+				parseInt(graphArray[i][3]),
+				parseInt(graphArray[i][4])
+			]);
+		}
 
 		var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
