5c9456105a1ccb444c512950c208e8a206826b26
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-07-26 14:02:49 +0200

    Setup new page

---
+++ view/diesel/advanced.php
@@ -0,0 +1,14 @@
+<html>  
+    <head>  
+        <title>  
+            Advanced
+        </title>  
+	</head>  
+	<link rel="stylesheet" href="/Webpage-Lsn/view/diesel/diesel.css" type="text/css">
+	<link rel="stylesheet" href="/Webpage-Lsn/view/diesel/popup.css" type="text/css">
+	<body>  
+		<h1>
+			Avanceret statistik for diesel forbrug
+		</h1>  
+	</body>
+</html>
