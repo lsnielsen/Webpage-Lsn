2d171f8c0ff9c044492683ce7db50652e6e5d115
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-07-28 10:12:26 +0200

    Update advanced.php

--- view/diesel/advanced.php
+++ view/diesel/advanced.php
@@ -106,40 +106,53 @@
 
 <script>
 	const table = document.querySelector('.firstTable'); //get the table to be sorted
+	
+	var km = 0;
+	var l = 0;
+	var kr = 0;
+	var kml = 0;
+	var kmkr = 0;
+	var kr/l = 0;
+	var kr/km = 0;
+	var lkr = 0;
+	var lkm = 0;
 
 	table.querySelectorAll('th') // get all the table header elements
-	  .forEach((element, columnNo)=>{ // add a click handler for each 
-		element.addEventListener('click', event => {
+		.forEach((element, columnNo)=>{ // add a click handler for each 
+			element.addEventListener('click', event => {
 			sortTable(table, columnNo); //call a function which sorts the table by a given column number
 		})
-	  })
-	  
-	  function sortTable(table, sortColumn){
-	  // get the data from the table cells
-	  const tableBody = table.querySelector('tbody')
-	  const tableData = table2data(tableBody);
-	  // sort the extracted data
-	  tableData.sort((a, b)=>{
-		if(a[sortColumn] > b[sortColumn]){
-		  return 1;
-		}
-		return -1;
-	  })
-	  // put the sorted data back into the table
-	  data2table(tableBody, tableData);
+	})
+
+
+
+	function sortTable(table, sortColumn){
+		// get the data from the table cells
+		const tableBody = table.querySelector('tbody')
+		const tableData = table2data(tableBody);
+		// sort the extracted data
+		tableData.sort((a, b)=>{
+			if(a[sortColumn] > b[sortColumn]){
+				return 1;
+			}
+			return -1;
+		})
+		// put the sorted data back into the table
+		data2table(tableBody, tableData);
 	}
-	  // this function gets data from the rows and cells 
+	
+	// this function gets data from the rows and cells 
 	// within an html tbody element
 	function table2data(tableBody){
-	  const tableData = []; // create the array that'll hold the data rows
-	  tableBody.querySelectorAll('tr')
+		const tableData = []; // create the array that'll hold the data rows
+		tableBody.querySelectorAll('tr')
 		.forEach(row=>{  // for each table row...
-		  const rowData = [];  // make an array for that row
-		  row.querySelectorAll('td')  // for each cell in that row
+			const rowData = [];  // make an array for that row
+			row.querySelectorAll('td')  // for each cell in that row
 			.forEach(cell=>{
-			  rowData.push(cell.innerText);  // add it to the row data
+				rowData.push(cell.innerText);  // add it to the row data
 			})
-		  tableData.push(rowData);  // add the full row to the table data 
+			tableData.push(rowData);  // add the full row to the table data 
 		});
 		console.log(tableData);
 		return tableData;
@@ -147,14 +160,14 @@
 
 	// this function puts data into an html tbody element
 	function data2table(tableBody, tableData){
-	  tableBody.querySelectorAll('tr') // for each table row...
+		tableBody.querySelectorAll('tr') // for each table row...
 		.forEach((row, i)=>{  
-		  const rowData = tableData[i-1]; // get the array for the row data
-		  row.querySelectorAll('td')  // for each table cell ...
+			const rowData = tableData[i-1]; // get the array for the row data
+			row.querySelectorAll('td')  // for each table cell ...
 			.forEach((cell, j)=>{
 				cell.innerText = rowData[j]; // put the appropriate array element into the cell
 			})
-		  tableData.push(rowData);
+			tableData.push(rowData);
 		});
 	}
 </script>
