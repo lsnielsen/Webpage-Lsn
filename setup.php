76c8a7396172b4639d1eb28c6d19eddda85fe5ee
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-07-29 20:00:57 +0200

    Move frontpage files

--- setup.php
+++ setup.php
@@ -1,14 +1,7 @@
 <?php
 
-$carButton = isset($_POST['carButton']) ? $_POST['carButton'] : "";
-$tvButton = isset($_POST['tvButton']) ? $_POST['tvButton'] : "";
 
-if ($carButton == "carPage") {
-	echo file_get_contents("view/car.html");
-} elseif ($tvButton == "tvPage") {
-	echo file_get_contents("view/tv.html");
-} else {
-	echo file_get_contents("view/frontpage.html");
-}
+echo file_get_contents("view/frontpage/frontpage.html");
+
 
 ?>
