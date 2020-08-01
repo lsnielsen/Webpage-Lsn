fd289d872ee109ab8e2edb231e17195e67b19273
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-08-01 19:38:51 +0200

    Style the included xml

--- view/politics/news.php
+++ view/politics/news.php
@@ -9,18 +9,56 @@
 	<body>
 		<h1>
 			Inkluderer her en xml fil
-		</h1>
-		<?php
-			include 'phpArticle.php';
-			$catalog = new SimpleXMLElement($xmlstr);
+		</h1>		
+			<table border="1">
+				<tr bgcolor="#9acd32">
+					<th>
+						Title
+					</th>
+					<th>
+						Artist
+					</th>
+					<th>
+						Country
+					</th>
+					<th>
+						Company
+					</th>
+					<th>
+						Price
+					</th>
+					<th>
+						Year
+					</th>
+				</tr>
+				<?php
+					include 'phpArticle.php';
+					$catalog = new SimpleXMLElement($xmlstr);
 
-			foreach ($catalog->xpath('//cd') as $cd) {
-				echo $cd->title, ' played by ', $cd->artist, '<br>';
-			}
-			echo $catalog->cd[0]->artist;
-			
-			
-		?>
+					foreach ($catalog->xpath('//cd') as $cd) {
+						echo 	"<tr>
+									<td>
+										$cd->title
+									</td>
+									<td>
+										$cd->artist
+									</td>
+									<td>
+										$cd->country
+									</td>
+									<td>
+										$cd->company
+									</td>
+									<td>
+										$cd->price
+									</td>
+									<td>
+										$cd->year
+									</td>
+								</tr>";
+					}					
+				?>
+			</table>
 		<br><br><br><br><br>
 		
 		
