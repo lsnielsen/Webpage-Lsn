62944a4e0f3a76d45b7e7fa24b44e6409b9b8f76
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-08-02 13:39:19 +0200

    Update politic site

--- view/politics/news.php
+++ view/politics/news.php
@@ -1,74 +1,61 @@
 <html lang="da">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
 	<head>
+		<link rel="stylesheet" type="text/css" href="css/backButton.css">
 		<title>
 			Nyheder
 		</title>
 	</head>
 	<body>
 		<h1>
-			Inkluderer her en xml fil
+			Nyheder og politik
 		</h1>		
-			<table border="1">
+	
+		<?php
+				include 'xml.php';
+				$rss = new SimpleXMLElement($xmlFeed);
+				echo "<div class=newsHeader>";
+				echo $rss->channel->title, ', ';
+				echo $rss->channel->link, ', ';
+				echo $rss->channel->description;
+				echo "</div>";
+		?>
+		<center>
+			<table border="1" class="xmlTable">
 				<tr bgcolor="#9acd32">
 					<th>
 						Title
 					</th>
 					<th>
-						Artist
+						Link
 					</th>
 					<th>
-						Country
-					</th>
-					<th>
-						Company
-					</th>
-					<th>
-						Price
-					</th>
-					<th>
-						Year
+						Description
 					</th>
 				</tr>
-				<?php
-					include 'phpArticle.php';
-					$catalog = new SimpleXMLElement($xmlstr);
-
-					foreach ($catalog->xpath('//cd') as $cd) {
+		<?php
+				foreach ($rss ->xpath('//channel') as $channel) {
+					foreach ($channel->item as $item) {
 						echo 	"<tr>
 									<td>
-										$cd->title
+										$item->title
 									</td>
 									<td>
-										$cd->artist
+										$item->link
 									</td>
 									<td>
-										$cd->country
-									</td>
-									<td>
-										$cd->company
-									</td>
-									<td>
-										$cd->price
-									</td>
-									<td>
-										$cd->year
+										$item->description
 									</td>
 								</tr>";
-					}					
-				?>
-			</table> <br><br><br>
-		<?php
-				include 'xmlNews.php';
-				$rss = new SimpleXMLElement($xmlFeed);
-
-				foreach ($rss ->xpath('//channel') as $channel) {
-					echo 	$channel->title;
+						
+					}
 				}
 		?>
+				</table> 
+		</center>
+		
 		<form action="/Webpage-Lsn/controller/politics.php" method="post">
-			<button id="backButton" type="submit"> 
+			<button id="politicsBackButton" type="submit"> 
 				Tilbage
 			</button>
 		</form>
@@ -76,7 +63,24 @@
 
 </html>
 
+<style>
+	.xmlTable {
+		margin-top: 20px;
+		width: 60%;
+	}
 
+	.newsHeader {
+		font-family: times, Times New Roman, times-roman, georgia, serif;
+		font-size: 48px;
+		line-height: 40px;
+		letter-spacing: -1px;
+		color: #444;
+		margin: 0 0 0 0;
+		padding: 0 0 0 0;
+		font-weight: 100;
+		text-align:center;
+	}
+</style>
 
 
 
