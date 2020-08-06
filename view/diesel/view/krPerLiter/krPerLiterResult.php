36d234d4574ba3941229a15a3750272fe898ba87
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-08-06 17:41:27 +0200

    Update krPerLiterResult.php

--- view/diesel/view/krPerLiter/krPerLiterResult.php
+++ view/diesel/view/krPerLiter/krPerLiterResult.php
@@ -1,17 +1,17 @@
 <table class="advancedTable bottomTable">
 		<tr>
 			<th class="dieselHeader" style="cursor: default"> </th>
-			<th class="dieselHeader" style="cursor: default">Liter</th>
 			<th class="dieselHeader" style="cursor: default">Kroner</th>
+			<th class="dieselHeader" style="cursor: default">Liter</th>
 			<th class="dieselHeader" style="cursor: default">Kr/l</th>
 		</tr>	<tr class="bottomRows">
 		<?php
 			echo "<td class=dieselTableCell> Gennemsnit </td>";
 			echo "<td class=dieselTableCell>";
-			echo number_format($graphArray[0]['averageLiter'], 2, ',', '.');
+			echo number_format($graphArray[0]['averageKr'], 2, ',', '.');
 			echo "</td>
 			<td class=dieselTableCell>";
-			echo number_format($graphArray[0]['averageKr'], 2, ',', '.');
+			echo number_format($graphArray[0]['averageLiter'], 2, ',', '.');
 			echo "</td>
 			<td class=dieselTableCell>";
 			echo number_format($graphArray[0]['averageKrPerLiter'], 2, ',', '.');
@@ -20,10 +20,10 @@
 			echo "	<tr class=bottomRows>";
 			echo "<td class=dieselTableCell> Samlet </td>";
 			echo "<td class=dieselTableCell>";
-			echo number_format($graphArray[0]['literSum'], 2, ',', '.');
+			echo number_format($graphArray[0]['krSum'], 2, ',', '.');
 			echo "</td>
 			<td class=dieselTableCell>";
-			echo number_format($graphArray[0]['krSum'], 2, ',', '.');
+			echo number_format($graphArray[0]['literSum'], 2, ',', '.');
 			echo "</td>
 			<td class=dieselTableCell>";
 			echo number_format($graphArray[0]['krPerLiterSum'], 2, ',', '.');
