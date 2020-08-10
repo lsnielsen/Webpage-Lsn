254e05a474b225587b57c846571e40371acccff8
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-08-11 01:30:40 +0200

    Update ownDieselGraph.php

--- view/diesel/view/ownDieselGraph.php
+++ view/diesel/view/ownDieselGraph.php
@@ -14,43 +14,117 @@
 		<center>
 		
 						<h2>
-							Bruger input
-								&nbsp;	&nbsp;	&nbsp;	&nbsp;
-							Simple beregninger
-								&nbsp;	&nbsp;	&nbsp;	&nbsp;
-							Avancerede beregninger
+							<div style="position: relative; right: 540px; bottom: 10px;">
+								Bruger input
+							</div>
+							<div style="position: relative; right: 310px; bottom: 35px;">
+								Simple beregninger
+							</div>
+							<div style="position: relative; left: 170px; bottom: 60px;">
+								Avancerede beregninger
+							</div>
 						</h2>
-						
-						<div class="dropdown" style="position: relative; right: 110px;">
-							<button class="dropbtn"> Indtastet værdier</button>
-							<div class="dropdown-content">
-								<a href="#">Kilometer</a>
-								<a href="#">Liter</a>
-								<a href="#">Kroner</a>
+						<div style="position: relative; bottom: 60px;">
+							<div class="dropdown" style="position: relative; right: 120px;">
+								<button class="dropbtn"> Indtastet værdier</button>
+								<div class="dropdown-content">
+									<a href="#">Kilometer</a>
+									<a href="#">Liter</a>
+									<a href="#">Kroner</a>
+								</div>
 							</div>
-						</div>
-						
-						<div class="dropdown" style="position: relative; right: 50px;">
-							<button class="dropbtn"> Beregnede værdier</button>
-							<div class="dropdown-content">
-								<a href="#">Kroner per liter</a>
-								<a href="#">Kilometer per liter</a>
-								<a href="#">Kilometer per kroner</a>
-								<a href="#">Liter per kilometer</a>
-								<a href="#">Liter per kroner</a>
-								<a href="#">Kroner per kilometer</a>
+							
+							<div class="dropdown" style="position: relative; right: 50px;">
+								<button class="dropbtn"> Beregnede værdier</button>
+								<div class="dropdown-content">
+									<a href="#">Kroner per liter</a>
+									<a href="#">Kilometer per liter</a>
+									<a href="#">Kilometer per kroner</a>
+									<a href="#">Liter per kilometer</a>
+									<a href="#">Liter per kroner</a>
+									<a href="#">Kroner per kilometer</a>
+								</div>
 							</div>
-						</div>
-						
-						<div class="dropdown" style="position: relative; left: 50px;">
-							<button class="dropbtn"> Statistiske værdier</button>
-							<div class="dropdown-content">
-								<a href="#">Kroner per liter</a>
-								<a href="#">Kilometer per liter</a>
-								<a href="#">Kilometer per kroner</a>
-								<a href="#">Liter per kilometer</a>
-								<a href="#">Liter per kroner</a>
-								<a href="#">Kroner per kilometer</a>
+							
+							<div class="dropdown" style="position: relative; left: 50px;">
+								<button class="dropbtn"> Kilometer </button>
+								<div class="dropdown-content">
+									<a href="#">Median</a>
+									<a href="#">Varians</a>
+									<a href="#">St. dev</a>
+								</div>
+							</div>
+							
+							<div class="dropdown" style="position: relative; left: 50px;">
+								<button class="dropbtn"> Liter </button>
+								<div class="dropdown-content">
+									<a href="#">Median</a>
+									<a href="#">Varians</a>
+									<a href="#">St. dev</a>
+								</div>
+							</div>
+							
+							<div class="dropdown" style="position: relative; left: 50px;">
+								<button class="dropbtn"> Kroner </button>
+								<div class="dropdown-content">
+									<a href="#">Median</a>
+									<a href="#">Varians</a>
+									<a href="#">St. dev</a>
+								</div>
+							</div>
+							
+							<div class="dropdown" style="position: relative; left: 50px;">
+								<button class="dropbtn"> Kr/l </button>
+								<div class="dropdown-content">
+									<a href="#">Median</a>
+									<a href="#">Varians</a>
+									<a href="#">St. dev</a>
+								</div>
+							</div>
+							
+							<div class="dropdown" style="position: relative; left: 50px;">
+								<button class="dropbtn"> Km/l </button>
+								<div class="dropdown-content">
+									<a href="#">Median</a>
+									<a href="#">Varians</a>
+									<a href="#">St. dev</a>
+								</div>
+							</div>
+							
+							<div class="dropdown" style="position: relative; left: 50px;">
+								<button class="dropbtn"> Km/kr </button>
+								<div class="dropdown-content">
+									<a href="#">Median</a>
+									<a href="#">Varians</a>
+									<a href="#">St. dev</a>
+								</div>
+							</div>
+							
+							<div class="dropdown" style="position: relative; left: 50px;">
+								<button class="dropbtn"> Kr/km </button>
+								<div class="dropdown-content">
+									<a href="#">Median</a>
+									<a href="#">Varians</a>
+									<a href="#">St. dev</a>
+								</div>
+							</div>
+							
+							<div class="dropdown" style="position: relative; left: 50px;">
+								<button class="dropbtn"> L/km </button>
+								<div class="dropdown-content">
+									<a href="#">Median</a>
+									<a href="#">Varians</a>
+									<a href="#">St. dev</a>
+								</div>
+							</div>
+							
+							<div class="dropdown" style="position: relative; left: 50px;">
+								<button class="dropbtn"> L/kr </button>
+								<div class="dropdown-content">
+									<a href="#">Median</a>
+									<a href="#">Varians</a>
+									<a href="#">St. dev</a>
+								</div>
 							</div>
 						</div>
 						
