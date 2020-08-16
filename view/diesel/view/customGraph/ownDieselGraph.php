3b774ba4938852c62e8d409a87de92f25526d5f8
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-08-16 14:40:11 +0200

    Add kilometer per liter

--- view/diesel/view/customGraph/ownDieselGraph.php
+++ view/diesel/view/customGraph/ownDieselGraph.php
@@ -11,6 +11,7 @@
 	<link rel="stylesheet" href="/Webpage-Lsn/view/diesel/css/customGraph/literPerKilometer.css" type="text/css">
 	<link rel="stylesheet" href="/Webpage-Lsn/view/diesel/css/customGraph/kronerPerKilometer.css" type="text/css">
 	<link rel="stylesheet" href="/Webpage-Lsn/view/diesel/css/customGraph/kilometerPerKroner.css" type="text/css">
+	<link rel="stylesheet" href="/Webpage-Lsn/view/diesel/css/customGraph/kilometerPerLiter.css" type="text/css">
 	
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -25,6 +26,7 @@
 				Valgte værdier:
 			</h2>
 			<div class="selectedVariables"> 
+				<?php include "../view/diesel/view/customGraph/selectFields/kilometerPerLiter.php" ?>
 				<?php include "../view/diesel/view/customGraph/selectFields/kilometerPerKroner.php" ?>
 				<?php include "../view/diesel/view/customGraph/selectFields/kronerPerKilometer.php" ?>
 				<?php include "../view/diesel/view/customGraph/selectFields/literPerKroner.php" ?>
