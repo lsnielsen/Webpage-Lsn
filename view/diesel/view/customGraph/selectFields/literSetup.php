a997708fb1ee402623a8b4e4607cf6075ac472a1
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-08-24 21:59:58 +0200

    Update literSetup.php

--- view/diesel/view/customGraph/selectFields/literSetup.php
+++ view/diesel/view/customGraph/selectFields/literSetup.php
@@ -13,93 +13,81 @@
 	var literPerKilometerAverage = [];
 	var literPerKronerAverage = [];
 	
-	console.log("liter");
 	for (var i = 0; i < arrayLength; i += 1) {
 		liter.push({ 
 			x: new Date(array[i][1]), 
 			y: array[i]['liter'] 
 		});
-		console.log(array[i]['liter']);
 	}
-	console.log("liter per kilometer:");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literPerKilometer.push({ 
 			x: new Date(array[i][1]), 
 			y: array[i]['l/km'] 
 		});
-		console.log(array[i]['l/km']);
 	}
-	console.log("liter per kroner:");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literPerKroner.push({ 
 			x: new Date(array[i][1]), 
 			y: array[i]['l/kr'] 
 		});
-		console.log(array[i]['l/kr']);
 	}
-	console.log("liter median");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literMedian.push({ 
 			x: new Date(array[i][1]), 
 			y: array[0]['literMedian'] 
 		});
-		console.log(array[0]['literMedian']);
 	}
-	console.log("liter varians");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literVarians.push({ 
 			x: new Date(array[i][1]), 
 			y: array[0]['literVariance'] 
 		});
-		console.log(array[0]['literVariance']);
 	}
-	console.log("liter standard Dev");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literStandardDev.push({ 
 			x: new Date(array[i][1]), 
 			y: array[0]['literStandardDev'] 
 		});
-		console.log(array[0]['literStandardDev']);
 	}
-	console.log("liter average");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literAverage.push({ 
 			x: new Date(array[i][1]), 
 			y: array[0]['averageLiter'] 
 		});
-		console.log(array[0]['averageLiter']);
 	}
-	console.log("liter per kilometer st dev");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literPerKilometerStDev.push({ 
 			x: new Date(array[i][1]), 
 			y: array[0]['averageLiter'] 
 		});
-		console.log(array[0]['averageLiter']);
 	}
-	console.log("liter kroner st dev");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literPerKronerStDev.push({ 
 			x: new Date(array[i][1]), 
 			y: array[0]['averageLiter'] 
 		});
-		console.log(array[0]['averageLiter']);
 	}
-	console.log("liter per kroner gennemsnit");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literPerKronerAverage.push({ 
 			x: new Date(array[i][1]), 
 			y: array[0]['averageLiterPerKr'] 
 		});
-		console.log(array[0]['averageLiterPerKr']);
 	}
-	console.log("liter per kilometer gennemsnit");
+
 	for (var i = 0; i < arrayLength; i += 1) {
 		literPerKilometerAverage.push({ 
 			x: new Date(array[i][1]), 
 			y: array[0]['averageLiterPerKm'] 
 		});
-		console.log(array[0]['averageLiterPerKm']);
 	}
 	
 	</script>
