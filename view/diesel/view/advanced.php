6202605db5218ef04af3d0e94114ded23f1e0056
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-08-26 13:48:22 +0200

    Update advanced.php

--- view/diesel/view/advanced.php
+++ view/diesel/view/advanced.php
@@ -31,34 +31,55 @@
 				<button class="dieselButton" 
 						value="dieselPage"
 						name="dieselButton"
-						style="left: -400px;">
+						style="left: -600px;">
 					Tilbage
+				</button>
+
+				<button class="dieselButton" 
+						value="kilometerPage"
+						name="dieselButton"
+						style="left: -400px;">
+					Statistik for km
+				</button>
+
+				<button class="dieselButton" 
+						value="literPage"
+						name="dieselButton"
+						style="left: -200px;">
+					Statistik for liter
+				</button>
+
+				<button class="dieselButton" 
+						value="kronerPage"
+						name="dieselButton">
+					Statistik for kr
 				</button>
 
 				<button class="dieselButton" 
 						value="krPerLiter"
 						name="dieselButton"
-						style="left: -200px;">
+						style="left: 200px;">
 					Statistik for kr/l
 				</button>
 
 				<button class="dieselButton" 
 						value="kmPerLiter"
-						name="dieselButton">
+						name="dieselButton"
+						style="left: 400px;">
 					Statistik for km/l
 				</button>
 				
 				<button class="dieselButton" 
 						value="kmPerKroner"
 						name="dieselButton"
-						style="left: 200px;">
+						style="left: 600px;">
 					Statistik for km/kr
 				</button>
 				
 				<button class="dieselButton" 
 						value="ownDieselGraph"
 						name="dieselButton"
-						style="left: 400px;">
+						style="left: 800px;">
 					Lav din egen graf
 				</button>
 			</form>
@@ -82,34 +103,55 @@
 				<button class="dieselButton" 
 						value="dieselPage"
 						name="dieselButton"
-						style="left: -400px;">
+						style="left: -600px;">
 					Tilbage
+				</button>
+
+				<button class="dieselButton" 
+						value="kilometerPage"
+						name="dieselButton"
+						style="left: -400px;">
+					Statistik for km
+				</button>
+
+				<button class="dieselButton" 
+						value="literPage"
+						name="dieselButton"
+						style="left: -200px;">
+					Statistik for liter
+				</button>
+
+				<button class="dieselButton" 
+						value="kronerPage"
+						name="dieselButton">
+					Statistik for kr
 				</button>
 
 				<button class="dieselButton" 
 						value="krPerLiter"
 						name="dieselButton"
-						style="left: -200px;">
+						style="left: 200px;">
 					Statistik for kr/l
 				</button>
 
 				<button class="dieselButton" 
 						value="kmPerLiter"
-						name="dieselButton">
+						name="dieselButton"
+						style="left: 400px;">
 					Statistik for km/l
 				</button>
 				
 				<button class="dieselButton" 
 						value="kmPerKroner"
 						name="dieselButton"
-						style="left: 200px;">
+						style="left: 600px;">
 					Statistik for km/kr
 				</button>
 				
 				<button class="dieselButton" 
 						value="ownDieselGraph"
 						name="dieselButton"
-						style="left: 400px;">
+						style="left: 800px;">
 					Lav din egen graf
 				</button>
 			</form>
