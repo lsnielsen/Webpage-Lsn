006e8d66c4cfc3d6a0d5dc0a747ef8dcfc45f2e9
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-08-28 21:11:42 +0200

    Start coding politics page

--- view/politics/parties.php
+++ view/politics/parties.php
@@ -2,6 +2,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<head>
 		<link rel="stylesheet" type="text/css" href="css/backButton.css">
+		<link rel="stylesheet" type="text/css" href="css/parties.css">
 		<title>
 			Folketinget
 		</title>
@@ -66,73 +67,6 @@
 
 </html>
 
-<style>
-
-
-
-
-	.politicTable {
-		border-radius: 16px;
-		overflow: hidden;
-		background: #7918f2;
-		background: -webkit-linear-gradient(-68deg, #ac32e4 , #4801ff);
-		background: -o-linear-gradient(-68deg, #ac32e4 , #4801ff);
-		background: -moz-linear-gradient(-68deg, #ac32e4 , #4801ff);
-		background: linear-gradient(-68deg, #ac32e4 , #4801ff);
-	}
-	table {
-		width: 100%;
-		background-color: #fff;
-	}
-
-	table {
-		background-color: transparent;
-	}
-
-	td {
-		font-family: Montserrat-Regular;
-		font-size: 14px;
-		color: #fff;
-		line-height: 1.4;
-	}
-
-	th {
-		font-family: Montserrat-Medium;
-		font-size: 12px;
-		color: #fff;
-		line-height: 1.4;
-		text-transform: uppercase;
-
-		background-color: rgba(255,255,255,0.32);
-	}
-	.hov-column {
-		background-color: rgba(255,255,255,0.1);
-	}
-
-	.row100:hover td {
-		background-color: rgba(255,255,255,0.1);
-	}
-
-	.row100 td:hover {
-		background-color: rgba(255,255,255,0.2);
-	}
-
-	th, td {
-		text-align: center; 
-		font-weight: unset;
-		padding-right: 10px;
-		padding-top: 24px;
-		padding-bottom: 20px;
-		width: 130px;
-		padding-left: 25px;
-	}
-
-	td {
-		padding-top: 18px;
-		padding-bottom: 14px;
-	}
-
-</style>
 
 
 
