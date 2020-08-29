ce85818445ce0861373c38b1adcbbb65db95ddf4
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-08-29 17:20:12 +0200

    Add new politic page

---
+++ view/politics/government.php
@@ -0,0 +1,36 @@
+
+<html>
+
+	<head>
+		<title>
+			Regeringen
+		</title>
+	</head>
+
+	<body style="background-color: blue;">
+		
+		<h1 style="text-align: center; font-size: 50px;">
+			Socialdemokratiet sidder i regeringen
+		</h1>
+		
+		<div style="position: relative; margin-left: 200px;">
+			<div style="font-size: 20px; font-weight: bold; margin-bottom: 10px;">
+				Her er Mette Frederiksens grundlovstale for 2020
+			</div>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/BXBLi-3szG8" 
+					frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; 
+					picture-in-picture" allowfullscreen>
+			</iframe>
+		</div>
+	</body>
+
+
+
+
+
+
+
+
+
+
+</html>
