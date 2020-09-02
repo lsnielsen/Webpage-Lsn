7561de076ba9a3cf4114b9845540e522e1757e11
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-02 15:41:48 +0200

    Update runChart.php

--- projects/running/graphs/runChart.php
+++ projects/running/graphs/runChart.php
@@ -52,21 +52,21 @@
                 axis.startValue = 0;
 				axis.axisThickness = 1;
                 axis.valueInterval = 1;
-                axis.endValue = 18;
+                axis.endValue = 15;
                 // color bands
                 var band1 = new AmCharts.GaugeBand();
                 band1.startValue = 0;
-                band1.endValue = 6;
+                band1.endValue = 5;
                 band1.color = "#00CC00";
 
                 var band2 = new AmCharts.GaugeBand();
-                band2.startValue = 6;
-                band2.endValue = 12;
+                band2.startValue = 5;
+                band2.endValue = 10;
                 band2.color = "#ffac29";
 
                 var band3 = new AmCharts.GaugeBand();
-                band3.startValue = 12;
-                band3.endValue = 18;
+                band3.startValue = 10;
+                band3.endValue = 15;
                 band3.color = "#ea3838";
 
                 axis.bands = [band1, band2, band3];
