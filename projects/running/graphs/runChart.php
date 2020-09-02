1968234ecc10821ddb0377b15724dc3f2d835c58
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-02 10:11:06 +0200

    Create runChart.php

---
+++ projects/running/graphs/runChart.php
@@ -0,0 +1,61 @@
+
+
+        <script>
+            var chart;
+            var arrow;
+            var axis;
+
+            AmCharts.ready(function () {
+                // create angular gauge
+                chart = new AmCharts.AmAngularGauge();
+                chart.addTitle("Speedometer");
+
+                // create axis
+                axis = new AmCharts.GaugeAxis();
+                axis.startValue = 0;
+				axis.axisThickness = 1;
+                axis.valueInterval = 1;
+                axis.endValue = 20;
+                // color bands
+                var band1 = new AmCharts.GaugeBand();
+                band1.startValue = 0;
+                band1.endValue = 7;
+                band1.color = "#00CC00";
+
+                var band2 = new AmCharts.GaugeBand();
+                band2.startValue = 7;
+                band2.endValue = 14;
+                band2.color = "#ffac29";
+
+                var band3 = new AmCharts.GaugeBand();
+                band3.startValue = 14;
+                band3.endValue = 20;
+                band3.color = "#ea3838";
+
+                axis.bands = [band1, band2, band3];
+
+                // bottom text
+                axis.bottomTextYOffset = -20;
+                axis.setBottomText("0 km/h");
+                chart.addAxis(axis);
+
+                // gauge arrow
+                arrow = new AmCharts.GaugeArrow();
+                chart.addArrow(arrow);
+
+                chart.write("chartdiv");
+                // change value every 2 seconds
+                setInterval(randomValue, 2000);
+            });
+
+            // set random value
+            function randomValue() {
+                var value = Math.round(Math.random() * 30);
+                arrow.setValue(value);
+                axis.setBottomText(value + " km/h");
+            }
+
+        </script>
+
+
+
