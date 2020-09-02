bc58d46828f71d1398c4bdedc435b8c576532caf
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-02 11:12:01 +0200

    Update runChart.php

--- projects/running/graphs/runChart.php
+++ projects/running/graphs/runChart.php
@@ -1,4 +1,38 @@
+<?php
 
+
+	$graphData = "SELECT * FROM running";
+	$query = mysqli_query($con,$graphData);
+	while ($rowArray = $query->fetch_array()) {
+		$graphArray[] = $rowArray;
+	}
+	echo "Start <br>";
+	echo "size of graphArray: " . sizeof($graphArray) . '<br> <br>';
+	for ($i=0; $i<sizeof($graphArray); $i++) {
+		echo "km: " . $graphArray[$i]['kilometer'] . '<br>';
+		echo "date: " . $graphArray[$i]['date'] . '<br>';
+		echo "time: " . $graphArray[$i]['time'] . '<br>';
+		echo "<br> <br>";
+		
+		$hour = $graphArray[$i]['time'][0] . $graphArray[$i]['time'][1];
+		$min = $graphArray[$i]['time'][3] . $graphArray[$i]['time'][4];
+		$sec = $graphArray[$i]['time'][6] . $graphArray[$i]['time'][7];
+		
+		echo "timer: " . $hour . '<br>';
+		echo "minutter: " . $min . '<br>';
+		echo "sekunder: " . $sec . '<br>';
+		
+		$min = $min + ($sec/60);
+		$hour = $hour + ($min/60);
+		
+		echo "timer: " . $hour . '<br>';
+
+		$kmPerHour = $graphArray[$i]['kilometer'] / $hour;
+
+		echo "km/t: " . $kmPerHour . '<br><br><br>';
+	}
+	echo "Done";
+?>
 
         <script>
             var chart;
