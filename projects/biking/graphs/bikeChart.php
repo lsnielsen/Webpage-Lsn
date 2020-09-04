14d1ad026bf18e7e7100e15b509adadcaf2b8eae
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-04 13:03:11 +0200

    Rewrite bike graph

--- projects/biking/graphs/bikeChart.php
+++ projects/biking/graphs/bikeChart.php
@@ -51,22 +51,22 @@
                 axis = new AmCharts.GaugeAxis();
                 axis.startValue = 0;
 				axis.axisThickness = 1;
-                axis.valueInterval = 1;
-                axis.endValue = 18;
+                axis.valueInterval = 3;
+                axis.endValue = 42;
                 // color bands
                 var band1 = new AmCharts.GaugeBand();
                 band1.startValue = 0;
-                band1.endValue = 6;
+                band1.endValue = 14;
                 band1.color = "#00CC00";
 
                 var band2 = new AmCharts.GaugeBand();
-                band2.startValue = 6;
-                band2.endValue = 12;
+                band2.startValue = 14;
+                band2.endValue = 28;
                 band2.color = "#ffac29";
 
                 var band3 = new AmCharts.GaugeBand();
-                band3.startValue = 12;
-                band3.endValue = 18;
+                band3.startValue = 28;
+                band3.endValue = 42;
                 band3.color = "#ea3838";
 
                 axis.bands = [band1, band2, band3];
@@ -87,32 +87,40 @@
 
 			var oldColor;
 			var oldId;
+			var oldValue;
+			var value;
+			var arrayLength;
+			var array;
 
             // set random value
-            function randomValue() {
-				var array = <?php echo json_encode($kmPerHourArray); ?>;
-				var arrayLength = array.length/2;
-                var value = Math.floor(Math.random() * arrayLength);
+            function randomValue() 
+			{
+				array = <?php echo json_encode($kmPerHourArray); ?>;
+				value = newValue();
                 arrow.setValue(array[value]);
                 axis.setBottomText(array[value] + " km/t");
 				
 				if (typeof(oldId) != "undefined" && oldId != null && typeof(oldColor) != "undefined" && oldColor != null) {
 					$("#" + oldId).css("background-color", oldColor);
 				}
-			//	console.log("value: " + value + ", oldId: " + oldId + ", oldColor: " + oldColor);
 
 				oldColor = $('#' + value).css("background-color");
-			//	console.log("value: " + value + ", oldId: " + oldId + ", oldColor: " + oldColor);
-
 				$("#" + value).css("background-color", "#4dd2ff");
 				oldId = value;
-				
-				
-				
-				
-			//	console.log("value: " + value + ", oldId: " + oldId + ", oldColor: " + oldColor);
-			//	console.log(" ");
             }
+			
+			function newValue()
+			{
+				console.log("newValue()");
+				arrayLength = array.length/2;
+                value = Math.floor(Math.random() * arrayLength);
+				while (typeof(oldValue) != "undefined" && oldValue != null && oldValue == value) {
+					console.log("while loop");
+					value = Math.floor(Math.random() * arrayLength);				
+				}
+				oldValue = value;
+				return value;
+			}
 
         </script>
 
