28cbcdee3b435490c1333ce5e8d43086ac12a4f2
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-08 14:07:19 +0200

    Update myfile.php

--- projects/usedCars/myfile.php
+++ projects/usedCars/myfile.php
@@ -7,7 +7,17 @@
 		$('#msgDiv').load('https://www.bilbasen.dk/');
     });
 
-	url = "https://www.bilbasen.dk/brugt/bil/audi/a6/30-tdi-272-avant-quattro-s-tr-5d/4761460"
+	url = "https://www.bilbasen.dk/brugt/bil/audi/a6/30-tdi-272-avant-quattro-s-tr-5d/4761460";
+	url = "https://www.bilbasen.dk/brugt/bil/Audi";
+	
+	
+	url = "https://www.bilbasen.dk/brugt/bil?IncludeEngrosCVR=true&PriceFrom=0&includeLeasing=true";
+	url = "https://www.bilbasen.dk/brugt/bil/vw";
+	url = "https://www.bilbasen.dk/brugt/bil/vw/passat";
+	url = "https://www.bilbasen.dk/brugt/bil/vw/passat/1,4_gte_variant_dsg_5d";
+	
+	// Denne url starter sådan: /brugt/bil/vw/passat/14-gte-variant-dsg-5d/4808684
+	url = "https://www.bilbasen.dk/brugt/bil/vw/passat/14-gte-variant-dsg-5d/4808684";
 	
 	$.get(url, 
 		function( data ) {
@@ -15,7 +25,8 @@
 			console.log(data);
 			//$('#msgDiv').load('https://www.bilbasen.dk/');
 		}, 
-	'html');  // or 'text', 'xml', 'more'
+		'html' // or 'text', 'xml', 'more'
+	);  
 
 
 </script>
