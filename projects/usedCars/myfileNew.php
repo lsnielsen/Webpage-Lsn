b42e7682467218f14acf7bb050369c38cf7a1799
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-10 16:11:46 +0200

    Update myfileNew.php

--- projects/usedCars/myfileNew.php
+++ projects/usedCars/myfileNew.php
@@ -10,14 +10,13 @@
 
 	var firstUrlArr = new Array();
 	var secondUrlArr = new Array();
+	var specificationArr = new Array();
 	
 	$( document ).ready(function() {
-		console.log("first click");
 		for(i = 1; i<2; i++) {
 			setTimeout(
 				function() 
 				{
-					console.log("call " + i);
 					if (i >= 2) {
 						urlTempOne = urlTempOne + "&page=" + i;
 					}
@@ -28,23 +27,34 @@
 		setTimeout(
 			function() 
 			{
-				$("#webscraperTwo").show();
+				$("#webscraperTwo").click();
 			}, 10000);
+	});
+	
+		
+	$("#webscraperTwo").click(function() {
+		console.log("First array:");
+		console.log(firstUrlArr);
+		for(i = 0; i<firstUrlArr.length; i++) {
+			callingSecondUrl(firstUrlArr[i]);
+		}
+		setTimeout(
+			function() 
+			{
+				console.log("Second array:");
+				console.log(secondUrlArr);
+			}, 3000);
 	});
 	
 	function callingFirstUrl(urlOne) 
 	{
 		$.get(urlOne, 
 			function( data ) {
-				//console.log("Datatype: " + typeof data);
-				//console.log("Datalength: " + data.length );
-				//console.log(" ");
 		
 				for ($i = 0; $i < data.length; $i++) {
 					subStr = data.substring($i, $i+60);
 					temp = subStr.search(/\"\/brugt\/bil\/[a-z]+\/[a-zA-Z0-9-,.\/]+\"$/);
 					if (temp != -1) {
-						//console.log("matching url");
 						var theFirstString = subStr.substring(temp + 1, subStr.length - 1);
 						theFirstString = "https://www.bilbasen.dk" + theFirstString;
 						firstUrlArr.push(theFirstString);
@@ -54,15 +64,7 @@
 			'html' // or 'text', 'xml', 'more'
 		);  
 	}		
-		
-	$("#webscraperTwo").click(function() {
-		console.log("Second click:");
-		console.log("Array of urls first:");
-		console.log(firstUrlArr);
-		for(i = 0; i<firstUrlArr.length; i++) {
-			callingSecondUrl(firstUrlArr[i]);
-		}
-	});
+	
 	
 	function callingSecondUrl(url)
 	{
@@ -75,20 +77,30 @@
 					tempTwo = subStr.search(/<td style="color: #888;">[a-zA-Z0-9.,-\/ ]+<\/td>/);
 					if (tempOne != -1 && tempTwo != -1) {
 						
-						var theString = subStr.substring(tempTwo, subStr.length);
-						theString = theString.replace(/<\/td>\n[ ]*<td>&nbsp;<\/td>\n[ ]*<td class="selectedcar">/,'');
-						//theString = theString.replace("<td style=\"color: #888;\">", "");
-						//theString = theString.replace("</td>", "");
-						//theString = theString.replace("</td>", "");
-						//theString = theString.replace("</td>", "");
-						//theString = theString.replace("<td>&nbsp;", "");
-						//theString = theString.replace("<td class=\"selectedcar\">", "");
-						console.log(theString);
-						console.log(" ");
-						secondUrlArr.push(theString);
+						var value = subStr.substring(tempTwo, subStr.length);
+						value = value.replace("<td style=\"color: #888;\">", "");
+						value = value.replace("</td>", "");
+						value = value.replace("</td>", "");
+						value = value.replace("</td>", "");
+						value = value.replace("<td>&nbsp;", "");
+						value = value.replace("<td class=\"selectedcar\">", "");
+						value = value.replace(/\n/, "");
+						value = value.replace(/\n/, "");
+						for(i=0; i<20; i++) {
+							value = value.replace(" ", "");
+						}
+						
+						value = value.substr(value.indexOf(" ") + 1);
+						value = value.replace(/ +/, "");
+						
+						//console.log(value);
+						secondUrlArr.push(value);
+						
 						$i = $i + 133;
 					}
 				}
+				secondUrlArr.push(url);
+				//console.log(" ");
 			},
 			'html'
 		);
@@ -116,82 +128,6 @@
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		$("#webscraperThree").click(function() {
-			console.log("Third click:");
-			console.log("Array of urls second:");
-			console.log(secondUrlArr);
-			for(var i in secondUrlArr) {
-				callingThirdUrl(secondUrlArr[i]);
-			}
-			setTimeout(
-				function() 
-				{
-					$("#webscraperFour").show();
-				}, 120000);
-		});
-		
-		$("#webscraperFour").click(function() {
-			console.log("Fourth click:");
-			console.log("Array of urls third:");
-			console.log(thirdUrlArr);
-			for(var i in thirdUrlArr) {
-				callingFourthUrl(thirdUrlArr[i]);
-			}			
-		});
-		
-	
-	function callingFourthUrl(fourthUrl) 
-	{
-		//console.log("last url: " + fourthUrl);
-		$.get(fourthUrl, 
-			function( data ) {
-				console.log("fourth url data: " + data);
-			//	console.log("third url ");
-				for ($i = 0; $i < data.length; $i++) {
-					subStr = data.substring($i, $i+60);
-					temp = subStr.search(/[a-zA-Z0-9.\/:]*\/brugt\/bil\/[a-z]+[\/]*[a-z]*\-*[a-z]+\-*[a-z]*\/*[a-zA-Z0-9,_]*\"$/);
-					if (temp != -1) {
-						var theFourthString = subStr.substring(temp + 1, subStr.length - 1);
-				//		console.log("matching url: " + theFourthString);
-						fourthUrlArr.push(theFourthString);
-					}
-				}
-			}, 
-			'html' // or 'text', 'xml', 'more'
-		);  
-	}
-	
-	
-	function callingThirdUrl(lastUrl)
-	{
-		//console.log("last url: " + lastUrl);
-		$.get(lastUrl, 
-			function( data ) {
-				//console.log("third url data: " + data);
-			//	console.log("third url ");
-				for ($i = 0; $i < data.length; $i++) {
-					subStr = data.substring($i, $i+60);
-					temp = subStr.search(/[a-zA-Z0-9.\/:]*\/brugt\/bil\/[a-z]+[\/]*[a-z]*\-*[a-z]+\-*[a-z]*\/*[a-zA-Z0-9,_]*\"$/);
-					if (temp != -1) {
-						var thethirdString = subStr.substring(temp + 1, subStr.length - 1);
-				//		console.log("matching url: " + thethirdString);
-						thirdUrlArr.push(thethirdString);
-					}
-				}
-			}, 
-			'html' // or 'text', 'xml', 'more'
-		);  
-	}
-	
 	
 	
 	
