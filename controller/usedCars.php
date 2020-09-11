4186bbd4cf154259d3f68adfffab9d0722429439
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-11 17:20:04 +0200

    Update usedCars.php

--- controller/usedCars.php
+++ controller/usedCars.php
@@ -2,21 +2,31 @@
 <?php
 
 		$usedCarsButton = isset($_POST['usedCarsArray']) ? $_POST['usedCarsArray'] : ""; 
-		//echo "used car button: " . $usedCarsButton;
+
 		if ($usedCarsButton != "") {
-			echo "do get here? <br>";
-			//echo '<pre>'; print_r($usedCarsButton); echo '</pre>';
+			
+			$dataArr;
+			$counter = 0;
 
-			//echo $usedCarsButton;
-
+			
 			$tempArr = explode(",", $usedCarsButton);
 			for($i=0; $i< sizeof($tempArr); $i++) {
 				if($tempArr[$i] != "") {
-					echo $tempArr[$i] . '<br>';
+					$tempValue = $tempArr[$i];
+					$dataArr[$counter][] = $tempValue;
+				}
+				if (preg_match('/https:/', $tempValue)) {
+					$counter++;
+				}					
+			}
+			for($i=0; $i< sizeof($dataArr); $i++) {
+				for($j=0; $j< sizeof($dataArr[$i]); $j++) {
+					echo $dataArr[$i][$j] . '<br>';
 				}
 			}
-			//echo '<pre>'; print_r($tempArr); echo '</pre>';
-			//echo $tempArr[93];
+			
+			
+			
 			
 		} else {
 			include("../projects/usedCars/frontpage.php");	
