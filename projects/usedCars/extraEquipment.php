11dc2e2344e58eb25f8a2a242bc47afc190c19a8
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-13 12:54:49 +0200

    Create extraEquipment.php

---
+++ projects/usedCars/extraEquipment.php
@@ -0,0 +1,94 @@
+
+
+<script>
+
+
+	function setExtraEquipment(data, singleCarArray)
+	{
+		equip = data.search("ABS-bremser");	
+		if(equip != -1) { singleCarArray['abs'] "Ja"; } else { singleCarArray['abs'] "-"; }
+		
+		equip = data.search("Alufælge");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Android auto");	
+		if(equip != -1) { singleCarArray['android'] "Ja"; } else { singleCarArray['android'] "-"; }
+		
+		equip = data.search("Anhængertræk");	
+		if(equip != -1) { singleCarArray['trækkrog'] "Ja"; } else { singleCarArray['trækkrog'] "-"; }
+		
+		equip = data.search("Anhængertræk, aftagl.");	
+		if(equip != -1) { singleCarArray['aftagl'] "Ja"; } else { singleCarArray['aftagl'] "-"; }
+		
+		equip = data.search("Antispin");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Apple carplay");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Armlæn");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Auto. nødbremse");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Auto. parkering");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Auto. start/stop");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Automatgear");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Automatisk lys");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("AUX tilslutning");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Bakkamera");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Bakspejl m. nedbl.");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Bi-xenon");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Blindvinkelassistent");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		equip = data.search("Bluetooth");	
+		if(equip != -1) { singleCarArray['alufælge'] "Ja"; } else { singleCarArray['alufælge'] "-"; }
+		
+		
+		
+		
+		
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
