cf29a70a6839cc52f64f5b100e1e57628f9182fa
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-16 11:50:49 +0200

    Update usedCarTable.php

--- projects/usedCars/usedCarTable.php
+++ projects/usedCars/usedCarTable.php
@@ -40,7 +40,7 @@
 									for($j=0; $j<31; $j++) {
 										if ($j == 0) {
 											echo "<td>";
-											echo "<a href='".$dataArr[$i][$j]." target=\"_blank\"'>Link til bilbasen</a>";
+											echo "<a href='".$dataArr[$i][$j]."' target=\"_blank\">Link til bilbasen</a>";
 											echo "</td>";
 										} else {
 											echo "<td>";
