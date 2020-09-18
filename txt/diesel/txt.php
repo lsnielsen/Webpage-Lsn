fce9007a9f02143da0ace94562df91ce79428c0f
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-18 20:27:19 +0200

    Make diesel frontpage

--- txt/diesel/txt.php
+++ txt/diesel/txt.php
@@ -3,9 +3,22 @@
 
 	return
 	[
-		'frontpage' => [
+		'frontpage' => 
+		[
 			'tabHeader' => 'Diesel',
-			'header' => 'Diesel page'
+			'header' => 'Diesel page',
+			'infoHeader' => 'Indtast dine tal nedenfor, og de vil blive vist i tabellen til højre',
+			'date' => 'Dato',
+			'km' => 'Antal kilometer',
+			'liter' => 'Liter',
+			'kr' => 'Kroner',
+			'enterNumbers' => 'Indsæt tal',
+			'tableHeader' => 'Tabel over indtastede diesel tal',
+			'deleteRow' => 'Slet række',
+			'confirmDelete' => 'Er du sikker på, at du vil slette denne række?',
+			'yes' => 'Ja',
+			'no' => 'Nej',
+			'backButton' => 'Tilbage til startside',
 		]
 
 
