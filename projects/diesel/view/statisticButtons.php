dc0eb1f2e887f52d43f4b10d9b790c317b6df728
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-18 21:29:22 +0200

    Update statisticButtons.php

--- projects/diesel/view/statisticButtons.php
+++ projects/diesel/view/statisticButtons.php
@@ -7,14 +7,15 @@
 						value="advancedDieselPage"
 						name="dieselButton"						
 						style="margin-left: auto;">
-					Klik her, for at se en mere avanceret statistik
+					<?php $txtFile = include("../txt/diesel/txt.php"); ?>
+					<?php $txtFile['statButtons']['statistic']; ?>
 				</button>
 				<button class="dieselButton" 
 						action="/../Webpage-Lsn/controller/diesel.php" 
 						value="carDieselPage"
 						name="dieselButton"						
 						style="margin-left: 100px; position: relative;">
-					Klik her, for at se hvilken bil det handler om
+					<?php $txtFile['statButtons']['theCar']; ?>
 				</button>	
 <?php
 	} else {
@@ -24,14 +25,14 @@
 						value="advancedDieselPage"
 						name="dieselButton"						
 						style="margin-left: 200px; position: relative; top: 10px;">
-					Klik her, for at se en mere avanceret statistik
+					<?php $txtFile['statButtons']['statistic']; ?>
 				</button>
 				<button class="dieselButton" 
 						action="/../Webpage-Lsn/controller/diesel.php" 
 						value="carDieselPage"
 						name="dieselButton"						
 						style="margin-left: 90px; position: relative; top: 10px;">
-					Klik her, for at se hvilken bil det handler om
+					<?php $txtFile['statButtons']['theCar']; ?>
 				</button>
 <?php
 	}
