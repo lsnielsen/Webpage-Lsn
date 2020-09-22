7325ff844528ce9825fa21bb868431dc3fb0767d
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-22 19:33:47 +0200

    Update global.php

--- text/global.php
+++ text/global.php
@@ -5,8 +5,9 @@
 
 <?php
 	
-	$languageVar = isset($_COOKIE["lang"]) ? $_COOKIE["lang"] : "";
-	if ($languageVar == "english") {
+	$langPlace = isset($_COOKIE["langPlace"]) ? $_COOKIE["langPlace"] : "";
+	$langVar = isset($_COOKIE["lang"]) ? $_COOKIE["lang"] : "";
+	if ($langVar == "english") {
 		return include("../text/english/diesel.php");
 	} else {
 		return include("../text/danish/diesel.php");
