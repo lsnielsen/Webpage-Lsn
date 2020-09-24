57270a7d0dee2e340c8aaaef605748e19f3cd38b
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-24 16:39:16 +0200

    Update frontpage.php

--- projects/usedCars/frontpage.php
+++ projects/usedCars/frontpage.php
@@ -57,7 +57,9 @@
 					</button>
 				</form>
 		
-		<?php 	echo "<h3> Antal biler fra 
+		<?php 	
+			if (isset($bilbasenCount) && isset($gulOgGratisCount)) {
+				echo "<h3> Antal biler fra 
 					<a href=\"https:\\www.bilbasen.dk\" target=\"_blank\">
 					www.bilbasen.dk </a> : $bilbasenCount </h3>"; 
 				echo "<h3> Antal biler fra 
@@ -65,6 +67,7 @@
 					www.guloggratis.dk </a>:  $gulOgGratisCount </h3>"; 
 				$sum = $bilbasenCount + $gulOgGratisCount;
 				echo "<h3> Samlet: $sum </h3>";
+			}
 		?>
 
 	</body>
