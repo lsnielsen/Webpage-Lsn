43ad4480cf3f30ab4f2e8c09fd94b7eca2b59aac
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-28 15:19:15 +0200

    Update usedCars.php

--- controller/usedCars.php
+++ controller/usedCars.php
@@ -77,10 +77,10 @@
 		$blankTest;
 		
 		for ($i=0; $i<sizeof($array); $i++) {
-			for ($j=31; $j<sizeof($array[$i]); $j++) {
-				if ($array[$i][$j] == "-" || !isset($blankTest[$j])) {
+			for ($j=28; $j<sizeof($array[$i]); $j++) {
+				if ($array[$i][$j] != "-") {
 					$blankTest[$j] = true;
-				} else {
+				} elseif ($array[$i][$j] == "-" && !isset($blankTest[$j])) {
 					$blankTest[$j] = false;
 				}
 			}
@@ -88,11 +88,14 @@
 		
 		
 		$keys = array_keys($blankTest); 
+		echo '<pre>'; print_r($blankTest); echo '</pre>';
+		echo '<pre>'; print_r($keys); echo '</pre>';
+		
 		for ($i=0; $i<sizeof($array); $i++) {
 			$columnCounter = 0;
 			for ($j=0; $j<sizeof($keys); $j++) {
-				if($blankTest[$keys[$j]] == true) {
-					//echo "columnCounter: " . $columnCounter . ", and j = " . $j . "<br>";
+				if($blankTest[$keys[$j]] == false) {
+					echo "columnCounter: " . $columnCounter . ", and j = " . $j . "<br>";
 					array_splice($array[$i], $keys[$j - $columnCounter], 1);
 					$columnCounter++;
 				}
@@ -101,7 +104,7 @@
 		$columnCounter = 0;
 
 		for ($j=0; $j<sizeof($keys); $j++) {
-			if($blankTest[$keys[$j]] == true) {
+			if($blankTest[$keys[$j]] == false) {
 				//echo "About to splice titleArray with j = " . $j . ", and keys[j] = " . $keys[$j] . " <br>";
 				array_splice($titleArray, $keys[$j-$columnCounter], 1);
 					$columnCounter++;
@@ -120,14 +123,6 @@
 	}
 	
 	
-		
-		
-		
-		
-		
-		
-		
-		
 		
 		
 		
