f5c71cb9c3552c3c28a588a5ce2e5374d02f43f4
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-29 10:29:45 +0200

    Update starter.php

--- projects/usedCars/gulOgGratis/starter.php
+++ projects/usedCars/gulOgGratis/starter.php
@@ -3,24 +3,15 @@
 
 	function callingSecondUrl(url)
 	{
-		//console.log("First url call ");
 		$.get(url, 
 			function( data ) {
-				//console.log("Gul og gratis data: " + data);
 				for ($i = 0; $i < data.length; $i++) {
 					subStr = data.substring($i, $i+200);
-					//"/biler/personbiler/audi/a6/annonce/48941394-audi-a6-limousine"
 					temp = subStr.search(/\"\/biler\/personbiler\/[a-z]+\/[a-zA-Z0-9-,.\/]+\/annonce\/[0-9a-zA-Z-]+\"$/);
 					if (temp != -1) {
 						var theFirstString = subStr.substring(temp + 1, subStr.length - 1);
-						//console.log("url: " + theFirstString);
-						//console.log("model: " + modelArray[0] + " " + modelArray[1]);
-						//console.log("true, includes: " + modelArray[0] + ", " + modelArray[1]);
 						theFirstString = "https://www.guloggratis.dk" + theFirstString;
-						//console.log("url: " + theFirstString);
-						//console.log(" ");
 						secondUrlArr.push(theFirstString);
-						//console.log("\n \n");
 					}
 				}
 			}, 
@@ -31,14 +22,11 @@
 
 		
 	function getTheUsedCarGulOgGratis() {
-		//console.log("First Gul og gratis array:");
-		//console.log(secondUrlArr);
 		
 		var loopJJ = 0;
 		mySecondLoop();
 		function mySecondLoop() {       
 			setTimeout(function() {   
-				//console.log("Making second url call");
 				getTheCarGulOgGratis(secondUrlArr[loopJJ]);
 				loopJJ++;                  
 				if (loopJJ < secondUrlArr.length) {         
@@ -52,10 +40,8 @@
 	
 	function getTheCarGulOgGratis(url)
 	{
-		//console.log("second url calls, url: " + url);
 		$.get(url, 
 			function( data ) {
-				//console.log("Gul og gratis car url: " + data);
 				var gogCarArray = new Array();
 
 				gogTheLink = url;
@@ -63,8 +49,6 @@
 				setPrimerGogAttributes(data, gogCarArray);
 				setExtraGogEquipment(data);
 				setTheFirstGogArray(gogCarArray);
-
-				//console.log("single car array : " + gogCarArray);
 			},
 			'html'
 		);
