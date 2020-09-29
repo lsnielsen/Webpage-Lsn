274cf196f793e641da3dec36b0721ba98616c6ff
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-29 11:46:13 +0200

    Update usedCarTable.php

--- projects/usedCars/usedCarTable.php
+++ projects/usedCars/usedCarTable.php
@@ -1,47 +1,51 @@
 		<table id="usedCars">
 						<tr>
 						<?php 
+							echo "<th>Nr: </th>";
 							foreach($headerArray as $attribute) {
 								echo "<th>" . $attribute . "</th>";
 							}
 						?>
 						</tr>
 						<?php
-						
+							$carCounter = 1;
 							if (isset($autoArr)) {
 								for($i=0; $i<sizeOf($autoArr); $i++) {
 									echo "<tr>";
+										echo "<td class=\"carCounterCell\"> $carCounter </td>";
 										for($j=0; $j<sizeOf($autoArr[$i]); $j++) {
 											if ($j == 0 && strpos($autoArr[$i][0], 'bilbasen') !== false) {
-												echo "<td>";
+												echo "<td class=\"carCell\">";
 												echo "<a href='".$autoArr[$i][$j]."' target=\"_blank\">Link til bilbasen</a>";
 												echo "</td>";
 											} elseif ($j == 0 && strpos($autoArr[$i][0], 'guloggratis') !== false) {
-												echo "<td>";
+												echo "<td class=\"carCell\">";
 												echo "<a href='".$autoArr[$i][$j]."' target=\"_blank\">Link til guloggratis</a>";
 												echo "</td>";
 											} else {
-												echo "<td>";
+												echo "<td class=\"carCell\">";
 												echo $autoArr[$i][$j];
 												echo "</td>";
 											}
 										}
 									echo "</tr>";
+									$carCounter++;
 								} 
 							}
 						
 							if (isset($manuelArr)) {
 								for($i=0; $i<sizeOf($manuelArr); $i++) {
 									echo "<tr>";
+										echo "<td class=\"carCounterCell\"> $carCounter </td>";
 										for($j=0; $j<sizeOf($manuelArr[$i]); $j++) {
 											if ($j == 0 && strpos($manuelArr[$i][0], 'bilbasen') !== false) {
-												echo "<td>";
+												echo "<td class=\"carCell\">";
 												echo "<a href='".$manuelArr[$i][$j]."' target=\"_blank\">Link til bilbasen</a>";
 												echo "</td>";
 											} elseif ($j == 0 && strpos($manuelArr[$i][0], 'guloggratis') !== false) {
-												echo "<td>";
+												echo "<td class=\"carCell\">";
 												echo "<a href='".$manuelArr[$i][$j]."' target=\"_blank\">Link til guloggratis</a>";
-												echo "</td>";
+												echo "</td class=\"carCell\">";
 											} else {
 												echo "<td>";
 												echo $manuelArr[$i][$j];
@@ -49,6 +53,7 @@
 											}
 										}
 									echo "</tr>";
+									$carCounter++;
 								} 
 							}
 							?>		
@@ -62,11 +67,18 @@
 	  width: 90%;
 	}
 
-	#usedCars td, #usedCars th {
-	  border: 1px solid #ddd;
-	  padding: 8px;
-          min-width: 120px;
-          text-align: center;
+	#usedCars .carCounterCell {
+		border: 1px solid #ddd;
+		padding: 8px;
+		min-width: 20px;
+		text-align: center;
+	}
+	
+	#usedCars .carCell {
+		border: 1px solid #ddd;
+		padding: 8px;
+		min-width: 120px;
+		text-align: center;
 	}
 
 	#usedCars tr:nth-child(even){background-color: #f2f2f2;}
