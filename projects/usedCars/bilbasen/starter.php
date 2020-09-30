57942181a5acf04b7d2d4b083d20285ef0b6d41c
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-09-30 14:13:11 +0200

    Update starter.php

--- projects/usedCars/bilbasen/starter.php
+++ projects/usedCars/bilbasen/starter.php
@@ -3,21 +3,19 @@
 
 	function callingFirstUrl(urlOne, modelArray) 
 	{
-            //console.log("First url call ");
             $.get(urlOne, 
                     function( data ) {
                             for ($i = 0; $i < data.length; $i++) {
                                     subStr = data.substring($i, $i+200);
-                                    //temp = subStr.search(/\"\/brugt\/bil\/[a-z]+\/[a-zA-Z0-9]+\/[a-zA-Z0-9-]+\/[0-9]+\"$/);
+
                                     temp = subStr.search(/href="\/brugt\/bil\/[a-z]+\/[a-zA-Z0-9-]+\/[a-zA-Z0-9-]+\/[0-9]+(\">)$/);
                                     if (temp != -1) {
                                             var theFirstString = subStr.substring(temp + 6, subStr.length - 2);
-                                            //console.log("url: " + theFirstString);
-                                            //console.log("model: " + modelArray[0] + " " + modelArray[1]);
-                                            //console.log("true, includes: " + modelArray[0] + ", " + modelArray[1]);
                                             theFirstString = "https://www.bilbasen.dk" + theFirstString;
-                                            //console.log("url: " + theFirstString);
-                                            firstUrlArr.push(theFirstString);
+
+											if (!firstUrlArr.includes(theFirstString)) {
+												firstUrlArr.push(theFirstString);
+											}
                                     }
                             }
                     }, 
@@ -27,16 +25,13 @@
 	
 		
 	function getTheUsedCarBilbasen() {
-            //console.log("First array:");
-            //console.log(firstUrlArr);
-
+		
             var loopII = 0;
             mySecondLoop();
             function mySecondLoop() {       
                     setTimeout(function() {   
-                            //console.log("Making second url call");
-                            getTheCarFromBilbasen(firstUrlArr[loopII]);
-                            loopII++;                  
+							getTheCarFromBilbasen(firstUrlArr[loopII]);
+							loopII++;                  
                             if (loopII < firstUrlArr.length) {         
                                     mySecondLoop();             
                             } else {  
@@ -51,10 +46,9 @@
 	
 	function getTheCarFromBilbasen(url)
 	{
-            //console.log("second url calls, url: " + url);
             $.get(url, 
                     function( data ) {
-                            //console.log("calling the second url");
+
                             var singleCarArray = new Array();
                             theLink = url;
                             getMainAttributes(data, singleCarArray);
@@ -62,7 +56,6 @@
                             setExtraEquipment(data);
 
                             setTheFirstArray(singleCarArray);
-                            //console.log("single car array : " + singleCarArray);
                     },
                     'html'
             );
