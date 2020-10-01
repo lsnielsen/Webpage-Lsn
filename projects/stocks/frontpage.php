b03b0b905cd4b522a4ccdc0584de06b8acf711aa
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-10-01 16:43:23 +0200

    Update frontpage.php

--- projects/stocks/frontpage.php
+++ projects/stocks/frontpage.php
@@ -1,6 +1,7 @@
 <html>
 
     <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
         <title>
@@ -8,11 +9,13 @@
         </title>
     </head>
 
-    <body class="p-3 mb-2 bg-secondary text-dark">
+    <body class="p-3 mb-2 bg-secondary text-dark" onload="getStockData()">
         <div class="jumbotron text-center">
             <h1>Her er en oversigt over kurserne på enkelte ting</h1>
             <p>Der er kurser og tilhørende statistik</p>
         </div>
+
+        <?php include "stockTable.php"; ?>
 
         <form action="/Webpage-Lsn/controller/frontpage.php" method="post">
             <button type="submit" class="btn btn-dark btn-lg active">
