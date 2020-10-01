955c63b2b01c078b615e5866adc6c638831a5623
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-10-01 10:50:17 +0200

    Update frontpage.php

---
+++ projects/stocks/frontpage.php
@@ -0,0 +1,18 @@
+<html>
+
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <title>
+            Aktie kurser
+        </title>
+    </head>
+
+    <body class="p-3 mb-2 bg-secondary text-dark">
+        <div class="jumbotron text-center">
+            <h1>Her er en oversigt over kurserne på enkelte ting</h1>
+            <p>Der er kurser og tilhørende statistik</p>
+        </div>
+    </body>
+
+</html>
