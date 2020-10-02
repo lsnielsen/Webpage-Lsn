15b0f015c858da81d9bd84a43ae049067a052c51
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-10-02 15:23:31 +0200

    Update oil.php

--- projects/stocks/assets/oil.php
+++ projects/stocks/assets/oil.php
@@ -10,8 +10,8 @@
             var url = "https://www.marketwatch.com/investing/fund/oil";
             $.get( url,
                 function( data ) {
-                    getOilValue(data);
-                    getOilPercentage(data);
+                    //getOilValue(data);
+                    //getOilPercentage(data);
                     getOilChange(data);
                 },
                 'html'
@@ -26,10 +26,10 @@
     {
         let oilRegex = /<bg-quote class="value" field="Last" format="0,0.00" channel="\/zigman2\/quotes\/204600377\/composite,\/zigman2\/quotes\/204600377\/lastsale">([0-9\.]+)<\/bg-quote>/;
         let oilMatch = oilRegex.exec(data);
+        let valueClose = /<span class="value">([0-9-%\.]+)<\/span>/.exec(data);
         if (oilMatch !== null) {
             $("#oilVal").text(oilMatch[1]);
-        } else {
-            let valueClose = /<span class="value">([0-9-%\.]+)<\/span>/.exec(data);
+        } else if (valueClose !== null) {
             $("#oilVal").text("Lukket: " + valueClose[1]);
         }
 
@@ -38,11 +38,11 @@
     {
         let oilRegex = /<bg-quote field="percentchange" format="0,0\.00%" channel="\/zigman2\/quotes\/[0-9]{9}\/[a-z\/"= ]+>([-0-9\.]+)%<\/bg-quote>/;
         let oilMatch = oilRegex.exec(data);
+        let percentageClose = /<span class="change--percent--q">([0-9-\.]+)%<\/span>/.exec(data);
         if (oilMatch !== null) {
             $("#oilPercentage").text(oilMatch[1] + " %");
             oilPercentageTxt(oilMatch, "#oilPercentage");
-        } else {
-            let percentageClose = /<span class="change--percent--q">([0-9-\.]+)%<\/span>/.exec(data);
+        } else if (percentageClose !== null) {
             $("#oilPercentage").text("Lukket: " + percentageClose[1] + " %");
             oilPercentageTxt(percentageClose, "#oilPercentage");
         }
@@ -51,21 +51,26 @@
     function oilPercentageTxt(match, field)
     {
         if (match[1] < 0) {
-            $(field).css("color", "red");
+            console.log("less than zero");
+            $("\"" + field + "\"").css("color", "red");
         } else {
+            console.log("larger than zero");
             $(field).css("color", "green");
         }
     }
 
     function getOilChange(data)
     {
-        let oilRegex = /<bg-quote field="change" format="0,0.00" channel="\/zigman2\/quotes\/204600377\/composite">([0-9\.-]+)<\/bg-quote>/;
+        console.log("oilChange");
+        let oilRegex = /<bg-quote field="change" format="0,0.00" channel="\/zigman2\/quotes\/204600377\/composite"[a-z=" ]+>([0-9\.-]+)<\/bg-quote>/;
         let oilMatch = oilRegex.exec(data);
+        let changeClose = /<span class="change--point--q">([0-9-%\.]+)<\/span>/.exec(data);
         if (oilMatch !== null) {
+            console.log("Match");
             $("#oilChange").text(oilMatch[1]);
             oilPercentageTxt(oilMatch, "#oilChange");
-        } else {
-            let changeClose = /<span class="change--point--q">([0-9-%\.]+)<\/span>/.exec(data);
+        } else if (changeClose !== null) {
+            console.log("No match");
             $("#oilChange").text("Lukket: " + changeClose[1]);
             oilPercentageTxt(changeClose, "#oilChange");
         }
