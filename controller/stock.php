fb64250c3aff19ff865e7527f2aa97f97e872a5c
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-10-03 20:14:43 +0200

    Update stock.php

--- controller/stock.php
+++ controller/stock.php
@@ -1,8 +1,11 @@
 <?php
 
     $stockButton = isset($_POST['stockButton']) ? $_POST['stockButton'] : null;
+    $stockCookie = isset($_COOKIE['stockButton']) ? $_COOKIE['stockButton'] : null;
 
     if ($stockButton == "stockPage") {
+        include "../projects/stocks/frontpage.php";
+    } elseif ($stockCookie == "bitcoin") {
         include "../projects/stocks/frontpage.php";
     }
 
