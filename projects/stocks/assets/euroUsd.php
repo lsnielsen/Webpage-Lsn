e1b049e5582c9afdb444e90ad575a12bc3fe7a73
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-10-03 08:43:38 +0200

    Update euroUsd.php

--- projects/stocks/assets/euroUsd.php
+++ projects/stocks/assets/euroUsd.php
@@ -26,8 +26,11 @@
     {
         let euroRegex = /<bg-quote class="value[" ]+[negative" ]* field="Last" format="0,0.00[0\[\]]*" channel="\/zigman2\/quotes\/[0-9]{9}\/[a-z\/0-9A-Z-\"=, ]+">([0-9\.,]+)<\/bg-quote>/;
         let euroMatch = euroRegex.exec(data);
+        let closeMatch = /<span class="value">([0-9\.-]+)<\/span>/.exec(data);
         if (euroMatch !== null) {
             $("#euroVal").text(euroMatch[1]);
+        } else if (closeMatch !== null) {
+            $("#euroVal").text(closeMatch[1]);
         }
     }
 
@@ -35,13 +38,13 @@
     {
         let euroRegex = /<bg-quote field="percentchange" format="0,0\.00%" channel="\/zigman2\/quotes\/[0-9]{9}\/[a-z\/"= ]+>([-0-9\.]+)%<\/bg-quote>/;
         let euroMatch = euroRegex.exec(data);
-        if (euroMatch !== null) {
+        let closeMatch = /<span class="change--percent--q">([0-9\.-]+)%<\/span>/.exec(data);
+        if (euroMatch !== null && closeMatch == null) {
             $("#euroPercentage").text(euroMatch[1] + " %");
-            if (euroMatch[1] < 0) {
-                $("#euroPercentage").css("color", "red");
-            } else {
-                $("#euroPercentage").css("color", "green");
-            }
+            textColor(euroMatch[1], "#euroPercentage");
+        } else {
+            $("#euroPercentage").text(closeMatch[1] + " %");
+            textColor(closeMatch[1], "#euroPercentage");
         }
     }
 
@@ -49,13 +52,22 @@
     {
         let euroRegex = /<bg-quote field="change" format="0,0\.00\[00\]" channel="\/zigman2\/quotes\/210561242\/realtime\/sampled"[ a-z0-9-="\.]*>([0-9\.-]*)<\/bg-quote>/;
         let euroMatch = euroRegex.exec(data);
+        let closeMatch = /<span class="change--point--q">([0-9-\.]+)<\/span>/.exec(data);
         if (euroMatch !== null) {
             $("#euroChange").text(euroMatch[1]);
-            if (euroMatch[1] < 0) {
-                $("#euroChange").css("color", "red");
-            } else {
-                $("#euroChange").css("color", "green");
-            }
+            textColor(euroMatch[1], "#euroChange");
+        } else if (closeMatch[1] !== null) {
+            $("#euroChange").text(closeMatch[1]);
+            textColor(closeMatch[1], "#euroChange");
+        }
+    }
+
+    function textColor(value, field)
+    {
+        if (value < 0) {
+            $(field).css("color", "red");
+        } else {
+            $(field).css("color", "green");
         }
     }
 
