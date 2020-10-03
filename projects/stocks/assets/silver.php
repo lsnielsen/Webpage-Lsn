51f4c78106b3e5178611e216f2d3435bd850807d
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-10-03 14:46:58 +0200

    Update silver.php

--- projects/stocks/assets/silver.php
+++ projects/stocks/assets/silver.php
@@ -26,8 +26,11 @@
     {
         let silverRegex = /<bg-quote class="value[" ]+[negative" ]* field="Last" format="0,0.00[0\[\]]*" channel="\/zigman2\/quotes\/[0-9]{9}\/[a-z\/0-9A-Z-\"=, ]+">([0-9\.,]+)<\/bg-quote>/;
         let silverMatch = silverRegex.exec(data);
+        let closeMatch = /<span class="value">([0-9\.-]+)<\/span>/.exec(data);
         if (silverMatch !== null) {
             $("#silverVal").text(silverMatch[1]);
+        } else if (closeMatch !== null) {
+            $("#silverVal").text(closeMatch[1]);
         }
     }
 
@@ -35,13 +38,13 @@
     {
         let silverRegex = /<bg-quote field="percentchange" format="0,0\.00%" channel="\/zigman2\/quotes\/[0-9]{9}\/[a-z\/"= ]+>([-0-9\.]+)%<\/bg-quote>/;
         let silverMatch = silverRegex.exec(data);
-        if (silverMatch !== null) {
+        let closeMatch = /<span class="change--percent--q">([0-9\.-]+)%<\/span>/.exec(data);
+        if (silverMatch !== null && closeMatch == null) {
             $("#silverPercentage").text(silverMatch[1] + " %");
-            if (silverMatch[1] < 0) {
-                $("#silverPercentage").css("color", "red");
-            } else {
-                $("#silverPercentage").css("color", "green");
-            }
+            textColor(silverMatch[1], "#silverPercentage");
+        } else if (closeMatch !== null) {
+            $("#silverPercentage").text(closeMatch[1] + " %");
+            textColor(closeMatch[1], "#silverPercentage");
         }
     }
 
@@ -49,13 +52,22 @@
     {
         let silverRegex = /<bg-quote field="change" format="0,0\.00\[0\]" channel="\/zigman2\/quotes\/[0-9]{9}\/delayed"[a-z=" ]*>([0-9\.-]+)<\/bg-quote>/;
         let silverMatch = silverRegex.exec(data);
+        let closeMatch = /<span class="change--point--q">([0-9-\.]+)<\/span>/.exec(data);
         if (silverMatch !== null) {
             $("#silverChange").text(silverMatch[1]);
-            if (silverMatch[1] < 0) {
-                $("#silverChange").css("color", "red");
-            } else {
-                $("#silverChange").css("color", "green");
-            }
+            textColor(silverMatch[1], "#silverChange");
+        } else if (closeMatch !== null) {
+            $("#silverChange").text(closeMatch[1]);
+            textColor(closeMatch[1], "#silverChange");
+        }
+    }
+
+    function textColor(value, field)
+    {
+        if (value < 0) {
+            $(field).css("color", "red");
+        } else {
+            $(field).css("color", "green");
         }
     }
 
