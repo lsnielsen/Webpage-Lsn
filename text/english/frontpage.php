e621287886275c6141c85c7f409c0318f6cd27e8
Lars <31955106+lsnielsen@users.noreply.github.com>
2020-10-06 13:37:06 +0200

    Make frontpage text

---
+++ text/english/frontpage.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+
+return
+    [
+        'tab' => 'LSN',
+        'frontHeader' => 'Welcome to the homepage - Lsn',
+        'projectTxt' => 'I have started various projects, which are linked to here:',
+        'dieselProject' => 'Diesel accounts',
+        'politicProject' => 'Politics',
+        'usedCarsProject' => 'Used cars',
+        'runProject' => 'Run',
+        'bikeProject' => 'Cycling',
+        'stockMarket' => 'Stock market',
+        'cvTxt' => 'My CV',
+        'searchPlaceholder' => 'Enter search form',
+        'searchButton' => 'Click to search using google',
+    ];
+
+
+
+
+
+
+
+
+
+
+?>
--- text/global.php
+++ text/global.php
@@ -7,10 +7,15 @@
 	
 	$langPlace = isset($_COOKIE["langPlace"]) ? $_COOKIE["langPlace"] : "";
 	$langVar = isset($_COOKIE["lang"]) ? $_COOKIE["lang"] : "";
-	if ($langVar == "english") {
-		return include("../text/english/diesel.php");
-	} else {
-		return include("../text/danish/diesel.php");
+
+    if ($langVar == "english" && $langPlace == "diesel") {
+        return include("../text/english/diesel.php");
+    } elseif ($langVar == "danish" && $langPlace == "diesel") {
+        return include("../text/danish/diesel.php");
+    } elseif ($langVar == "danish" && $langPlace == "frontpage") {
+        return include("../text/danish/frontpage.php");
+    } elseif ($langVar == "english" && $langPlace == "frontpage") {
+		return include("../text/english/frontpage.php");
 	}
 	
 	
