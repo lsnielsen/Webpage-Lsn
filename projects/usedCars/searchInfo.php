7126ffa03503631bde1858aa0b4374c2f69dfce9
Lars <lars.sondertoft@gmail.com>
2021-01-13 16:24:24 +0100

    Update searchInfo.php

--- projects/usedCars/searchInfo.php
+++ projects/usedCars/searchInfo.php
@@ -10,26 +10,31 @@
         <div class="form-group col-sm-2 pt-3">
             <select class="carModel custom-select form-select form-select-lg mb-3" aria-label=".form-select-lg">
                 <option> <?php echo $txtFile['chooseModel']; ?> </option>
-                <option class="dropdown-item" style="font-size: 30px;" disabled>Volvo</option>
+                <option class="dropdown-item" style="font-size: 18px; font-weight: bold;" disabled>Volvo: </option>
                 <option class="dropdown-item" value="Volvo V60">Volvo V60</option>
                 <option class="dropdown-item" value="Volvo XC40">Volvo XC40</option>
                 <option class="dropdown-item" value="Volvo V40">Volvo V40</option>
                 <option class="dropdown-item" value="Volvo V40%20CC V40CC">Volvo V40 CC</option>
-                <h5 class="dropdown-header">Volkswagen</h5>
+
+                <option class="dropdown-item" style="font-size: 18px; font-weight: bold;" disabled>Volkswagen: </option>
                 <option class="dropdown-item" value="vw t-roc">Vw T-roc</option>
-                <h5 class="dropdown-header">Audi</h5>
+
+                <option class="dropdown-item" style="font-size: 18px; font-weight: bold;" disabled>Audi: </option>
                 <option class="dropdown-item" value="Audi A3">Audi A3</option>
                 <option class="dropdown-item" value="Audi A4">Audi A4</option>
                 <option class="dropdown-item" value="Audi A6">Audi A6</option>
-                <h5 class="dropdown-header">Saab</h5>
+
+                <option class="dropdown-item" style="font-size: 18px; font-weight: bold;" disabled>Saab: </option>
                 <option class="dropdown-item" value="Saab 9-3">Saab 9-3</option>
                 <option class="dropdown-item" value="Saab 9-5">Saab 9-5</option>
-                <h5 class="dropdown-header">Ford</h5>
+
+                <option class="dropdown-item" style="font-size: 18px; font-weight: bold;" disabled>Ford: </option>
                 <option class="dropdown-item" value="Ford Fiesta">Ford Fiesta</option>
-                <h5 class="dropdown-header">Bmw</h5>
+
+                <option class="dropdown-item" style="font-size: 18px; font-weight: bold;" disabled>Bmw: </option>
                 <option class="dropdown-item" value="BMW ms-3-Serie">Bmw 3-serie</option>
                 <option class="dropdown-item" value="BMW ms-2-Serie">Bmw 2-serie</option>
-                <h5 class="dropdown-header">Peugeot</h5>
+                <option class="dropdown-item" style="font-size: 18px; font-weight: bold" disabled>Peugeot: </option>
                 <option class="dropdown-item" value="Peugeot 206">Peugeot 206+</option>
             </select>
         </div>
