d32f652c238ce6055935c533c4a173ddfecd337c
Lars <lars.sondertoft@gmail.com>
2021-01-13 15:21:19 +0100

    Update searchInfo.php

--- projects/usedCars/searchInfo.php
+++ projects/usedCars/searchInfo.php
@@ -6,30 +6,31 @@
                 Her kan du vælge hvilken bilmodel du gerne vil se nærmere på:
             </h3>
         </div>
-        <div class="col-sm-2">
-            <h3>
-                <form class="modelDropdown">
-                    <select class="carModel frontpageStyle">
-                        <option style="font-size: 20px;">
-                            <?php echo $txtFile['chooseModel']; ?>
-                        </option>
-                        <option class="carModelOptions" value="Volvo V60">Volvo V60</option>
-                        <option class="carModelOptions" value="Volvo XC40">Volvo XC40</option>
-                        <option class="carModelOptions" value="Volvo V40">Volvo V40</option>
-                        <option class="carModelOptions" value="vw t-roc">Vw T-roc</option>
-                        <option class="carModelOptions" value="Volvo V40%20CC V40CC">Volvo V40 CC</option>
-                        <option class="carModelOptions" value="Audi A3">Audi A3</option>
-                        <option class="carModelOptions" value="Audi A4">Audi A4</option>
-                        <option class="carModelOptions" value="Audi A6">Audi A6</option>
-                        <option class="carModelOptions" value="Saab 9-3">Saab 9-3</option>
-                        <option class="carModelOptions" value="Saab 9-5">Saab 9-5</option>
-                        <option class="carModelOptions" value="Ford Fiesta">Ford Fiesta</option>
-                        <option class="carModelOptions" value="BMW ms-3-Serie">Bmw 3-serie</option>
-                        <option class="carModelOptions" value="BMW ms-2-Serie">Bmw 2-serie</option>
-                        <option class="carModelOptions" value="Peugeot 206">Peugeot 206+</option>
-                    </select>
-                </form>
-            </h3>
+        <div class="form-group col-sm-3 pt-3">
+            <select class="carModel custom-select form-select form-select-lg mb-3" aria-label=".form-select-lg">
+                <option> <?php echo $txtFile['chooseModel']; ?> </option>
+                <h5 class="dropdown-header">Volvo</h5>
+                <option class="dropdown-item" value="Volvo V60">Volvo V60</option>
+                <option class="dropdown-item" value="Volvo XC40">Volvo XC40</option>
+                <option class="dropdown-item" value="Volvo V40">Volvo V40</option>
+                <option class="dropdown-item" value="Volvo V40%20CC V40CC">Volvo V40 CC</option>
+                <h5 class="dropdown-header">Volkswagen</h5>
+                <option class="dropdown-item" value="vw t-roc">Vw T-roc</option>
+                <h5 class="dropdown-header">Audi</h5>
+                <option class="dropdown-item" value="Audi A3">Audi A3</option>
+                <option class="dropdown-item" value="Audi A4">Audi A4</option>
+                <option class="dropdown-item" value="Audi A6">Audi A6</option>
+                <h5 class="dropdown-header">Saab</h5>
+                <option class="dropdown-item" value="Saab 9-3">Saab 9-3</option>
+                <option class="dropdown-item" value="Saab 9-5">Saab 9-5</option>
+                <h5 class="dropdown-header">Ford</h5>
+                <option class="dropdown-item" value="Ford Fiesta">Ford Fiesta</option>
+                <h5 class="dropdown-header">Bmw</h5>
+                <option class="dropdown-item" value="BMW ms-3-Serie">Bmw 3-serie</option>
+                <option class="dropdown-item" value="BMW ms-2-Serie">Bmw 2-serie</option>
+                <h5 class="dropdown-header">Peugeot</h5>
+                <option class="dropdown-item" value="Peugeot 206">Peugeot 206+</option>
+            </select>
         </div>
         <div class="col-sm-2">
             <h3>
