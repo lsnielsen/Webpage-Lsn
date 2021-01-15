77abc7581b8de4243677934ad69977000f7c04fc
Lars <lars.sondertoft@gmail.com>
2021-01-15 12:31:38 +0100

    Update dropdown.php

--- projects/usedCars/dropdown.php
+++ projects/usedCars/dropdown.php
@@ -2,66 +2,70 @@
 
 
 
-<div class="dropdownContainer form-group col-sm-2 pt-3">
-    <select class="carModel custom-select form-select form-select-lg mb-3" aria-label=".form-select-lg">
-        <option> <?php echo $txtFile['chooseModel']; ?> </option>
-        <option class="dropdown-item header" disabled>Volvo: </option>
-        <option class="dropdown-item" value="Volvo V60">Volvo V60</option>
-        <option class="dropdown-item" value="Volvo XC40">Volvo XC40</option>
-        <option class="dropdown-item" value="Volvo V40">Volvo V40</option>
-        <option class="dropdown-item" value="Volvo V40%20CC V40CC">Volvo V40 CC</option>
-
-        <option class="dropdown-item header" disabled>Volkswagen: </option>
-        <option class="dropdown-item" value="vw t-roc">Vw T-roc</option>
-
-        <option class="dropdown-item header" disabled>Audi: </option>
-        <option class="dropdown-item" value="Audi A3">Audi A3</option>
-        <option class="dropdown-item" value="Audi A4">Audi A4</option>
-        <option class="dropdown-item" value="Audi A6">Audi A6</option>
-
-        <option class="dropdown-item header" disabled>Saab: </option>
-        <option class="dropdown-item" value="Saab 9-3">Saab 9-3</option>
-        <option class="dropdown-item" value="Saab 9-5">Saab 9-5</option>
-
-        <option class="dropdown-item header" disabled>Ford: </option>
-        <option class="dropdown-item" value="Ford Fiesta">Ford Fiesta</option>
-
-        <option class="dropdown-item header" disabled>Bmw: </option>
-        <option class="dropdown-item" value="BMW ms-3-Serie">Bmw 3-serie</option>
-        <option class="dropdown-item" value="BMW ms-2-Serie">Bmw 2-serie</option>
-
-        <option class="dropdown-item header" disabled>Peugeot: </option>
-        <option class="dropdown-item" value="Peugeot 206">Peugeot 206+</option>
-    </select>
-</div>
 
 <form class="modelDropdown">
     <select class="carModel frontpageStyle">
-        <option style="font-size: 20px;"> <?php echo $txtFile['chooseModel']; ?> </option>
-        <option class="carModelOptions" value="Volvo V60">Volvo V60</option>
-        <option class="carModelOptions" value="Volvo XC40">Volvo XC40</option>
-        <option class="carModelOptions" value="Volvo V40">Volvo V40</option>
-        <option class="carModelOptions" value="vw t-roc">Vw T-roc</option>
-        <option class="carModelOptions" value="Volvo V40%20CC V40CC">Volvo V40 CC</option>
-        <option class="carModelOptions" value="Audi A3">Audi A3</option>
-        <option class="carModelOptions" value="Audi A4">Audi A4</option>
-        <option class="carModelOptions" value="Audi A6">Audi A6</option>
-        <option class="carModelOptions" value="Saab 9-3">Saab 9-3</option>
-        <option class="carModelOptions" value="Saab 9-5">Saab 9-5</option>
-        <option class="carModelOptions" value="Ford Fiesta">Ford Fiesta</option>
-        <option class="carModelOptions" value="BMW ms-3-Serie">Bmw 3-serie</option>
-        <option class="carModelOptions" value="BMW ms-2-Serie">Bmw 2-serie</option>
-        <option class="carModelOptions" value="Peugeot 206">Peugeot 206+</option>
+        <option style="font-size: 20px;">
+            <?php echo $txtFile['chooseModel']; ?>
+        </option>
+
+        <option class="subheader" disabled> Volvo: </option>
+        <option class="carOption" value="Volvo V60">Volvo V60</option>
+        <option class="carOption" value="Volvo XC40">Volvo XC40</option>
+        <option class="carOption" value="Volvo V40">Volvo V40</option>
+        <option class="carOption" value="Volvo V40%20CC V40CC">Volvo V40 CC</option>
+
+        <option class="subheader" disabled>Volkswagen: </option>
+        <option class="carOption" value="vw t-roc">Vw T-roc</option>
+
+        <option class="subheader" disabled>Audi: </option>
+        <option class="carOption" value="Audi A3">Audi A3</option>
+        <option class="carOption" value="Audi A4">Audi A4</option>
+        <option class="carOption" value="Audi A6">Audi A6</option>
+
+        <option class="subheader" disabled>Saab: </option>
+        <option class="carOption" value="Saab 9-3">Saab 9-3</option>
+        <option class="carOption" value="Saab 9-5">Saab 9-5</option>
+
+        <option class="subheader" disabled>Ford: </option>
+        <option class="" value="Ford Fiesta">Ford Fiesta</option>
+
+        <option class="subheader" disabled>Bmw: </option>
+        <option class="carOption" value="BMW ms-3-Serie">Bmw 3-serie</option>
+        <option class="carOption" value="BMW ms-2-Serie">Bmw 2-serie</option>
+
+        <option class="subheader" disabled>Peugeot: </option>
+        <option class="carOption" value="Peugeot 206">Peugeot 206+</option>
     </select>
 </form>
 
 <style>
-    .header {
-        font-size: 20px;
-        font-weight: bold;
+    .subheader {
+        font-size: 21px;
+        text-align: center;
+        padding-left: 10px;
     }
-    .dropdownContainer {
-        padding-left: 40px;
-        padding-right: 40px;
+    .carOption {
+        padding-left: -10px;
+    }
+    .frontpageStyle {
+        color: #fff !important;
+        text-transform: uppercase;
+        text-decoration: none;
+        background: #ed3330;
+        padding: 20px;
+        border-radius: 5px;
+        display: inline-block;
+        border: none;
+        transition: all 0.4s ease 0s;
+        margin-left: 100px;
+    }
+    .frontpageStyle:hover {
+        background: #434343;
+        letter-spacing: 1px;
+        -webkit-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+        -moz-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+        box-shadow: 5px 40px -10px rgba(0,0,0,0.57);
+        transition: all 0.4s ease 0s;
     }
 </style>
