2a61c546c2ff9eb8937842dc97e399f5b6b8d495
Lars <lars.sondertoft@gmail.com>
2021-01-17 17:33:21 +0100

    Update usedCars.php

--- text/danish/usedCars.php
+++ text/danish/usedCars.php
@@ -2,28 +2,28 @@
 
 <?php
 
-return [
-    'tabTitle' => 'Brugte biler',
-    'linkTxt' => 'Her kan du downloade en excel fil med brugte biler fra',
-    'linkOne' => 'bilbasen.dk',
-    'oneMore' => 'og',
-    'linkTwo' => 'guloggratis.dk.',
-    'resultTxt' => 'Resultatet vil også blive vist i en tabel nedenfor.',
-    'searchOne' => 'Du er nu gået igang med at søge efter biler på bilbasen, som har modellen',
-    'searchTwo' => 'Du skal væbne dig med lidt tålmodighed, da det tager lidt tid. Mellem 1 og 2 minutter, alt afhængig af antallet af biler.',
-    'searchThree' => 'Vi har nu hentet alle links til bilbasen. Vi fandt ',
-    'searchFour' => 'biler hos bilbasen, og',
-    'searchFive' => 'biler ved guloggratis.',
-    'searchFinish' => 'Nu er alle bilerne hentet fra bilbasen, så nu bliver de vist for dig, om cirka 2 sek.',
-    'dropdownTxt' => 'Her kan du vælge hvilken bilmodel du gerne vil se nærmere på:',
-    'chooseModel' => 'Vælg bilmodel',
-    'back' => 'Tilbage til forsiden',
-    'nrOfCars' => 'Antal biler fra',
-    'bilbasenLink' => 'www.bilbasen.dk',
-    'guloggratisLink' => 'www.guloggratis.dk',
-    'sumCars' => 'Sum:',
-
-]
+    return
+    [
+        'tabTitle' => 'Brugte biler',
+        'linkTxt' => 'Her kan du downloade en excel fil med brugte biler fra',
+        'linkOne' => 'bilbasen.dk',
+        'oneMore' => 'og',
+        'linkTwo' => 'guloggratis.dk.',
+        'resultTxt' => 'Resultatet vil også blive vist i en tabel nedenfor.',
+        'searchOne' => 'Du er nu gået igang med at søge efter biler på bilbasen, som har modellen',
+        'searchTwo' => 'Du skal væbne dig med lidt tålmodighed, da det tager lidt tid. Mellem 1 og 2 minutter, alt afhængig af antallet af biler.',
+        'searchThree' => 'Vi har nu hentet alle links til bilbasen. Vi fandt ',
+        'searchFour' => 'biler hos bilbasen, og',
+        'searchFive' => 'biler ved guloggratis.',
+        'searchFinish' => 'Nu er alle bilerne hentet fra bilbasen, så nu bliver de vist for dig, om cirka 2 sek.',
+        'dropdownTxt' => 'Her kan du vælge hvilken bilmodel du gerne vil se nærmere på:',
+        'chooseModel' => 'Vælg bilmodel',
+        'back' => 'Tilbage til forsiden',
+        'nrOfCars' => 'Antal biler fra ',
+        'bilbasenLink' => 'www.bilbasen.dk',
+        'guloggratisLink' => 'www.guloggratis.dk',
+        'sumCars' => 'Sum:',
+    ]
 
 
 
