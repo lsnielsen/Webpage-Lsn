051d125cfedd187dd52a90e6fc57a0b7ab29a080
Lars <lars.sondertoft@gmail.com>
2021-01-22 14:15:47 +0100

    Update starter.php

--- projects/usedCars/bilbasen/starter.php
+++ projects/usedCars/bilbasen/starter.php
@@ -3,24 +3,24 @@
 
 	function callingFirstUrl(urlOne)
 	{
-            $.get(urlOne, 
-                    function( data ) {
-                            for ($i = 0; $i < data.length; $i++) {
-                                    let subStr = data.substring($i, $i+200);
+        $.get(urlOne,
+            function( data ) {
+                for ($i = 0; $i < data.length; $i++) {
+                    let subStr = data.substring($i, $i+200);
 
-                                    let temp = subStr.search(/href="\/brugt\/bil\/[a-z]+\/[a-zA-Z0-9-]+\/[a-zA-Z0-9-]+\/[0-9]+(\">)$/);
-                                    if (temp != -1) {
-                                        let theFirstString = subStr.substring(temp + 6, subStr.length - 2);
-                                        theFirstString = "https://www.bilbasen.dk" + theFirstString;
+                    let temp = subStr.search(/href="\/brugt\/bil\/[a-z]+\/[a-zA-Z0-9-]+\/[a-zA-Z0-9-]+\/[0-9]+(\">)$/);
+                    if (temp != -1) {
+                        let theFirstString = subStr.substring(temp + 6, subStr.length - 2);
+                        theFirstString = "https://www.bilbasen.dk" + theFirstString;
 
-											if (!firstUrlArr.includes(theFirstString)) {
-												firstUrlArr.push(theFirstString);
-											}
-                                    }
-                            }
-                    }, 
-                    'html' // or 'text', 'xml', 'more'
-            );  
+                        if (!firstUrlArr.includes(theFirstString)) {
+                            firstUrlArr.push(theFirstString);
+                        }
+                    }
+                }
+            },
+            'html' // or 'text', 'xml', 'more'
+        );
 	}		
 	
 		
@@ -30,16 +30,16 @@
         mySecondLoop();
         function mySecondLoop() {
             setTimeout(function() {
-                    getTheCarFromBilbasen(firstUrlArr[loopII]);
-                    loopII++;
-                    if (loopII < firstUrlArr.length) {
-                        mySecondLoop();
-                    } else {
-                        setTimeout(function() {
-                            lastBool = true;
-                        }, 3000)
-                    }
-                }, 120)
+                getTheCarFromBilbasen(firstUrlArr[loopII]);
+                loopII++;
+                if (loopII < firstUrlArr.length) {
+                    mySecondLoop();
+                } else {
+                    setTimeout(function() {
+                        lastBool = true;
+                    }, 3000)
+                }
+            }, 120)
         }
 	}
 	
