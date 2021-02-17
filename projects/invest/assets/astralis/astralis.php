1dbb4a01e5fc329880bd28d0372c85a64102f078
Lars <lars.sondertoft@gmail.com>
2021-02-17 15:23:09 +0100

    Update astralis.php

--- projects/invest/assets/astralis/astralis.php
+++ projects/invest/assets/astralis/astralis.php
@@ -16,14 +16,13 @@
             $.get( url,
                 function( data ) {
                     getAstralisValue(data);
-                    getAstralisPercentage(data);
                     setStandardData();
                 },
                 'html'
             );
             setTimeout(function () {
                 callUrl();
-            }, 200);
+            }, 1200);
         }
     }
 
@@ -42,37 +41,24 @@
         let astralisMatch = astralisRegex.exec(data);
         let closeMatch = /<span class="value">([0-9\.-]+)<\/span>/.exec(data);
         if (astralisMatch !== null) {
-            $("#astralisVal").text(astralisMatch[1]);
-            let totalValue = ((astralisMatch[1] * astralisStocks) - astralisPrice).toFixed(2);
-            let stockValue = (astralisMatch[1] - pricePerStock).toFixed(2);
-            $("#astralisResult").text(totalValue);
-            textColor(totalValue, "#astralisResult");
-            $("#astralisStockResult").text(stockValue);
-            textColor(stockValue, "#astralisStockResult");
+            setData(astralisMatch[1]);
         } else if (closeMatch !== null) {
-            $("#astralisVal").text(closeMatch[1]);
-            let totalValue = ((closeMatch[1] * astralisStocks) - astralisPrice).toFixed(2);
-            let stockValue = (astralisMatch[1] - pricePerStock).toFixed(2);
-            $("#astralisResult").text(totalValue);
-            textColor(totalValue, "#astralisResult");
-            $("#astralisStockResult").text(stockValue);
-            textColor(stockValue, "#astralisStockResult");
+            setData(closeMatch[1]);
         }
     }
 
-    function getAstralisPercentage(data)
+    function setData(startValue)
     {
-        let astralisRegex = /<bg-quote field="percentchange" format="0,0\.00%" channel="\/zigman2\/quotes\/[0-9]{9}\/[a-z\/"= ]+>([-0-9\.]+)%<\/bg-quote>/;
-        let astralisMatch = astralisRegex.exec(data);
-        let closeMatch = /<span class="change--percent--q">([0-9\.-]+)%<\/span>/.exec(data);
-        if (astralisMatch !== null && closeMatch == null) {
-            $("#astralisPercentage").text(astralisMatch[1] + " %");
-            textColor(astralisMatch[1], "#astralisPercentage");
-        } else if (closeMatch !== null) {
-            $("#astralisPercentage").text(closeMatch[1] + " %");
-            textColor(closeMatch[1], "#astralisPercentage");
-        }
-    }
+        $("#astralisVal").text(startValue);
+        let totalValue = ((startValue * astralisStocks) - astralisPrice).toFixed(2);
+        let stockValue = (startValue - pricePerStock).toFixed(2);
+        let percentageValue = (((startValue / pricePerStock) * 100) - 100).toFixed(2);
+        $("#astralisPercentage").text(percentageValue + " %");
+        $("#astralisResult").text(totalValue);
+        $("#astralisStockResult").text(stockValue);
+        textColor(percentageValue, "#astralisPercentage");
+        textColor(totalValue, "#astralisResult");
+        textColor(stockValue, "#astralisStockResult");}
 
     function textColor(value, field)
     {
