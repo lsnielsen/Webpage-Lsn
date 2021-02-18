73229c39a904c5c78519b2fa97bc7e06b0e18678
Lars <lars.sondertoft@gmail.com>
2021-02-18 15:50:13 +0100

    Update danishResult.php

--- projects/invest/danish/danishResult.php
+++ projects/invest/danish/danishResult.php
@@ -1,20 +1,23 @@
 
 
 
-<table class="table table-hover table-dark table-bordered">
-    <thead>
-        <tr>
-            <th scope="col"></th>
-            <th scope="col">Købspris</th>
-            <th scope="col">Gevinst \ tab</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Samlet</td>
-            <td></td>
-            <td></td>
-        </tr>
-    </tbody>
-</table>
-
+<div class="container-sm">
+    <table class="table table-bordered align-middle table-primary">
+        <thead>
+            <tr>
+                <th scope="col"></th>
+                <th scope="col">Købspris</th>
+                <th scope="col">Nuværende pris</th>
+                <th scope="col">Gevinst \ tab</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">Samlet</th>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
