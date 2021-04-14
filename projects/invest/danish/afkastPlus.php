fdd703ab909fa4f64fb52ad6bdf815f6fd03d6f1
Lars <lars.sondertoft@gmail.com>
2021-04-14 20:04:05 +0200

    Create afkastPlus.php

---
+++ projects/invest/danish/afkastPlus.php
@@ -0,0 +1,74 @@
+
+
+<script>
+
+    let afkastPlusDate = "9/1 - 2021";
+    let afkastPlusPrice = 8000.00;
+    let afkastPlusName = "AfkastPlus Group";
+    let afkastPlusStocks = 1210;
+    let pricePerStock = (afkastPlusPrice / afkastPlusStocks).toFixed(2);
+
+    function getAfkastPlusData()
+    {
+        callUrl();
+        function callUrl() {
+            const url = "https://www.morningstar.dk/dk/funds/snapshot/snapshot.aspx?id=F000010QNK";
+            $.get( url,
+                function( data ) {
+                    getAfkastPlusValue(data);
+                    setAfkastPlusStandardData();
+                },
+                'html'
+            );
+            setTimeout(function () {
+                callUrl();
+            }, 5000);
+        }
+    }
+
+    function setAfkastPlusStandardData()
+    {
+        $("#afkastPlusDate").text(afkastPlusDate);
+        $("#afkastPlusPrice").text(afkastPlusPrice);
+        $("#afkastPlusName").text(afkastPlusName);
+        $("#afkastPlusStocks").text(afkastPlusStocks);
+        $("#afkastPlusStockPrice").text(pricePerStock);
+    }
+
+    function getAfkastPlusValue(data)
+    {
+        let afkastPlusRegex = /<bg-quote class="value[" ]+[negative" ]* field="Last" format="0,0.00[0\[\]]*" channel="\/zigman2\/quotes\/[0-9]{9}\/[a-z\/0-9A-Z-\"=, ]+">([0-9\.,]+)<\/bg-quote>/;
+        let afkastPlusMatch = afkastPlusRegex.exec(data);
+        let closeMatch = /<span class="value">([0-9\.-]+)<\/span>/.exec(data);
+        if (afkastPlusMatch !== null) {
+            setData(afkastPlusMatch[1]);
+        } else if (closeMatch !== null) {
+            setData(closeMatch[1]);
+        }
+    }
+
+    function setData(startValue)
+    {
+        $("#afkastPlusVal").text(startValue);
+        let totalValue = ((startValue * afkastPlusStocks) - afkastPlusPrice).toFixed(2);
+        let stockValue = (startValue - pricePerStock).toFixed(2);
+        let percentageValue = (((startValue / pricePerStock) * 100) - 100).toFixed(2);
+        $("#afkastPlusPercentage").text(percentageValue + " %");
+        $("#afkastPlusResult").text(totalValue);
+        $("#afkastPlusStockResult").text(stockValue);
+        textColor(percentageValue, "#afkastPlusPercentage");
+        textColor(totalValue, "#afkastPlusResult");
+        textColor(stockValue, "#afkastPlusStockResult");
+    }
+
+    function textColor(value, field)
+    {
+        if (value < 0) {
+            $(field).css("color", "red");
+        } else {
+            $(field).css("color", "green");
+        }
+    }
+
+
+</script>
